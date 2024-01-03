19c09ba235ee3247e5ad69f3419eb2f8f14c430d
ehiosu <ehiandbrothers@gmail.com>
2024-01-03 16:22:56 +0100

    update styling

--- datr/src/CPD/Layout/CPDLayout.jsx
+++ datr/src/CPD/Layout/CPDLayout.jsx
@@ -3,9 +3,9 @@ import { Sidebar } from '../Sidebar/Sidebar'
 import { Outlet } from 'react-router'
 export const CPDLayout = () => {
   return (
-    <main   className='flex w-full bg-my-gray'>
+    <main   className='flex w-full bg-my-gray max-w-screen-2xl'>
         <Sidebar/>
-       <div className="lg:w-[80%] w-full mx-auto">
+       <div className="lg:w-[75%] w-full mx-auto  overflow-x-hidden">
          <Outlet/>
        </div>
     </main>
