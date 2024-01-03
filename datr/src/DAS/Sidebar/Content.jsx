9bc66124c263df583625850c2ef24b31fc3ba947
ehiosu <ehiandbrothers@gmail.com>
2024-01-03 16:40:41 +0100

    add data and statistics module sidebar content file

---
+++ datr/src/DAS/Sidebar/Content.jsx
@@ -0,0 +1,15 @@
+import { AiOutlineStock } from "react-icons/ai";
+import { HiOutlineTicket } from "react-icons/hi";
+import {BiMessageSquareDetail} from "react-icons/bi"
+import {TbReportSearch} from 'react-icons/tb'
+import {FiSettings} from 'react-icons/fi'
+import {BsPersonPlus} from 'react-icons/bs'
+import { BsClipboard2Data } from "react-icons/bs";
+export const content =[
+    {
+        Name:'Terminals',
+        Icon : <AiOutlineStock className="text-2xl"/>,
+        SubMenu:[],
+        to :'/CPD/DAS'
+    },
+]
