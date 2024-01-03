9fa3b6d7f7e07e268af123fbfe0e549b788abbc3
ehiosu <ehiandbrothers@gmail.com>
2024-01-03 16:16:04 +0100

    styling fix

--- datr/src/CPD/Components/RecentTickets.jsx
+++ datr/src/CPD/Components/RecentTickets.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import {RecentTicketsTable}   from   './DataTable'
+import React from "react";
+import { RecentTicketsTable } from "./DataTable";
+import { useNavigate } from "react-router";
 export const RecentTickets = () => {
   return (
-    <div className='w-full h-full bg-white'>
-        <RecentTicketsTable/>
+    <div className="w-full h-full bg-white">
+      <RecentTicketsTable />
     </div>
-  )
-}
-
+  );
+};
