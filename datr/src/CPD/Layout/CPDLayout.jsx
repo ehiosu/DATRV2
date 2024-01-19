6dd8a35a8b5955b600a237a91c164e882c8e9097
ehiosu <ehiandbrothers@gmail.com>
2024-01-19 17:00:14 +0100

    Update cpd layout component styling

--- datr/src/CPD/Layout/CPDLayout.jsx
+++ datr/src/CPD/Layout/CPDLayout.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Sidebar } from '../Sidebar/Sidebar'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { Outlet } from "react-router";
 export const CPDLayout = () => {
   return (
-    <main   className='flex w-full bg-my-gray max-w-screen-2xl'>
-        <Sidebar/>
-       <div className="lg:w-[75%] w-full mx-auto  overflow-x-hidden">
-         <Outlet/>
-       </div>
+    <main className="flex w-full bg-my-gray max-w-screen-2xl ">
+      <Sidebar />
+      <div className="xl:w-[75%] lg:w-[85%] w-full mx-auto  overflow-x-hidden">
+        <Outlet />
+      </div>
     </main>
-  )
-}
+  );
+};
