98f2b8445c43eed8146dff0927c1c2f3490482fe
ehiosu <ehiandbrothers@gmail.com>
2024-02-02 11:32:54 +0100

    add ticket request type complete component

--- datr/src/CPD/Components/TicketRequestType.jsx
+++ datr/src/CPD/Components/TicketRequestType.jsx
@@ -1,5 +1,9 @@
 import React, { useRef } from "react";
-import { AiOutlineArrowDown, AiOutlinePlus } from "react-icons/ai";
+import {
+  AiOutlineArrowDown,
+  AiOutlineClose,
+  AiOutlinePlus,
+} from "react-icons/ai";
 import { requestEntries } from "../data/data.tsx";
 import {
   Collapsible,
@@ -13,14 +17,63 @@ import {
   DialogTrigger,
 } from "../../components/ui/dialog.tsx";
 import { Check } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogTrigger,
+} from "../../components/ui/alert-dialog.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select.tsx";
+import { Input } from "../../components/ui/input.tsx";
 export const TicketRequestType = () => {
   return (
     <div className="w-full flex flex-col gap-4">
       <p className="text-[1rem] ml-2 font-semibold ">Request Type</p>
-      <button className="w-max px-4 flex gap-3 items-center bg-blue-300 rounded-md text-white h-max py-2">
-        Add New Request
-        <AiOutlinePlus className="text-[0.95rem]" />
-      </button>
+      <AlertDialog>
+        <AlertDialogTrigger className="w-max px-4 flex gap-3 items-center bg-blue-300 rounded-md text-white h-max py-2">
+          Add New Request
+          <AiOutlinePlus className="text-[0.95rem]" />
+        </AlertDialogTrigger>
+        <AlertDialogContent className="flex flex-col">
+          <AlertDialogCancel className="w-max ml-auto border-none">
+            <AiOutlineClose />
+          </AlertDialogCancel>
+          <div className="flex items-center gap-2">
+            <p className="md:w-[20%] text-darkBlue font-semibold text-[0.8275rem]">
+              Category
+            </p>
+            <Select defaultValue="airline_problems">
+              <SelectTrigger>
+                <SelectValue placeholder="Select Category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="airline_problems">
+                  Airline Problems
+                </SelectItem>
+                <SelectItem value="service issues">Service Issues</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <p className="md:w-[20%] text-darkBlue font-semibold text-[0.8275rem]">
+              Request
+            </p>
+            <Input
+              placeholder="Rude Staff"
+              className="dark:focus:outline-transparent dark:focus:ring-transparent dark:focus-within:ring-transparent dark:focus:ring-offset-transparent "
+            />
+          </div>
+          <button className="block h-8 mt-4 bg-darkBlue text-white hover:cursor-pointer hover:bg-purple-400 transition-colors hover:ring-2  hover:ring-offset-4  rounded-md hover:ring-purple-300">
+            Submit
+          </button>
+        </AlertDialogContent>
+      </AlertDialog>
       <div className="w-full max-h-[50vh] transition-all overflow-y-auto">
         <div className="pl-10 pr-3 flex justify-between items-center bg-neutral-300 py-2">
           <p className="text-[0.8275rem] font-bold">Request Type</p>
