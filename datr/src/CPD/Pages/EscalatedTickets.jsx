38cf76ac4e270a3dbaa8b87e9483b2afaa9cfa62
ehiosu <ehiandbrothers@gmail.com>
2024-02-02 11:47:18 +0100

    fix document formatting

--- datr/src/CPD/Pages/EscalatedTickets.jsx
+++ datr/src/CPD/Pages/EscalatedTickets.jsx
@@ -1,75 +1,79 @@
-import React from 'react'
-import { SearchPage } from '../../Reusable/SearchPage'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
-import { AiOutlineArrowDown } from 'react-icons/ai'
-import {   EscalatedTicketsTable } from '../Components/DataTable'
+import React from "react";
+import { SearchPage } from "../../Reusable/SearchPage";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { EscalatedTicketsTable } from "../Components/DataTable";
 
 export const EscalatedTickets = () => {
   return (
-    <section    className='w-full'>
-        <SearchPage heading={'All Tickets'}>
-            <div className="w-full  flex-wrap   flex">
-                <div className="flex    gap-4   items-center">
-                    <ViewChangeBtn/>
-                    <button className='w-44 bg-darkBlue rounded-md  text-white  h-10' >New Application</button>
-                </div>
-                <div className="ml-auto flex    gap-3">
-                    <FilterButton/>
-                    <RangeSelectButton/>
-                </div>
-            </div>
-            <div className="w-full    max-h-[75vh]  mt-4 overflow-y-auto ">
-                <EscalatedTicketsTable/>
-            </div>
-        </SearchPage>
+    <section className="w-full">
+      <SearchPage heading={"Escalated Tickets"}>
+        <div className="w-full  flex-wrap   flex">
+          <div className="flex    gap-4   items-center">
+            <ViewChangeBtn />
+            <button className="w-44 bg-darkBlue rounded-md  text-white  h-10">
+              New Application
+            </button>
+          </div>
+          <div className="ml-auto flex    gap-3 md:my-0 my-2 flex-wrap">
+            <FilterButton />
+            <RangeSelectButton />
+          </div>
+        </div>
+        <div className="w-full    max-h-[75vh]  mt-4 overflow-y-auto ">
+          <EscalatedTicketsTable />
+        </div>
+      </SearchPage>
     </section>
-  )
-}
+  );
+};
 
-const  ViewChangeBtn=()=>{
-    return(
-        <DropdownMenu>
-                        <DropdownMenuTrigger  className='bg-[#EEEEEE] flex justify-center items-center  w-24 outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   text-[0.8275rem]'  >
-                                Views   <AiOutlineArrowDown/>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent    className=' bg-white w-28   rounded-md     z-50 my-2'>
-                        <DropdownMenuItem   className='h-6'>
-                            <p className="text-[0.8275rem] text-neutral-400    text-center">
-                                List
-                            </p>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator  className='bg-neutral-500'/>
-                        <DropdownMenuItem   className='h-6'>
-                            <p className="text-[0.8275rem] text-neutral-400    text-center">
-                                Grid
-                            </p>
-                        </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-    )
-}
-
-const  FilterButton=()=>{
-    return(
-        <DropdownMenu>
-        <DropdownMenuTrigger  className='bg-[#EEEEEE] flex justify-center items-center  w-32   text-[0.8275rem]  outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   '  >
-                Filter by  All   <AiOutlineArrowDown/>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent    className=' bg-white w-28   rounded-md     z-50 my-2'>
-
-        </DropdownMenuContent>
+const ViewChangeBtn = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="bg-[#EEEEEE] flex justify-center items-center  w-24 outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   text-[0.8275rem]">
+        Views <AiOutlineArrowDown />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className=" bg-white w-28   rounded-md     z-50 my-2">
+        <DropdownMenuItem className="h-6">
+          <p className="text-[0.8275rem] text-neutral-400    text-center">
+            List
+          </p>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="bg-neutral-500" />
+        <DropdownMenuItem className="h-6">
+          <p className="text-[0.8275rem] text-neutral-400    text-center">
+            Grid
+          </p>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
-    )
-}
-const RangeSelectButton=()=>{
-    return(
-        <DropdownMenu>
-        <DropdownMenuTrigger  className='border-darkBlue    border-2 flex justify-center items-center  w-56   text-[0.8275rem]  outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   '  >
-                Showing Last  90  days of Data <AiOutlineArrowDown/>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent    className=' bg-white w-28   rounded-md     z-50 my-2'>
+  );
+};
 
-        </DropdownMenuContent>
+const FilterButton = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="bg-[#EEEEEE] flex justify-center items-center  w-32   text-[0.8275rem]  outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   ">
+        Filter by All <AiOutlineArrowDown />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className=" bg-white w-28   rounded-md     z-50 my-2"></DropdownMenuContent>
     </DropdownMenu>
-    )
-}
+  );
+};
+const RangeSelectButton = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="border-darkBlue    border-2 flex justify-center items-center  w-56   text-[0.8275rem]  outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   ">
+        Showing Last 90 days of Data <AiOutlineArrowDown />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className=" bg-white w-28   rounded-md     z-50 my-2"></DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
