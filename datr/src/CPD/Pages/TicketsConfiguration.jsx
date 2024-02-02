6ec611b3e2e7072b57a3418e7d47d71ab46bae51
ehiosu <ehiandbrothers@gmail.com>
2024-02-02 11:52:15 +0100

    revamp ticket configuration page

--- datr/src/CPD/Pages/TicketsConfiguration.jsx
+++ datr/src/CPD/Pages/TicketsConfiguration.jsx
@@ -1,34 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { TabsContent } from "@radix-ui/react-tabs";
 import { motion, AnimatePresence } from "framer-motion";
+import { TicketRequestType } from "../Components/TicketRequestType";
+import { TicketStatusTypes } from "../Components/TicketStatusTypes";
+import { TicketAlerts } from "../Components/TicketAlerts";
 
 export const TicketsConfiguration = () => {
-    const [activeIndex,setActiveIndex]=useState(0)
-    useEffect(()=>{
-
-    })
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <TabsContent value="tickets">
     <AnimatePresence>
       <motion.section
         initial={{ scale: 0.1, opacity: 0 }}
         exit={{ scale: 0.1, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
-        className="w-[90%]  mx-auto max-h-[80vh] md:overflow-y-auto flex p-2 gap-2"
+        className="w-[90%]  mx-auto min-h-[80vh]  max-h-[80vh] md:overflow-y-auto flex md:flex-row flex-col p-2 gap-4 "
       >
-    <aside className="w-48 relative p-3 outline bg-white">
-        <div className="relative h-6 w-full grid place-items-center text-[0.9rem] font-semibold text-">
-            <span className={'absolute  font-semibold'}>Request Types</span>
-        </div>
-        <div className="relative h-6 w-full grid place-items-center my-2">
-            <span className={'absolute  '}>Status Types</span>
-        </div>
-        <div className="relative h-6 w-full grid place-items-center my-2">
-            <span className={'absolute  '}>Alerts</span>
-        </div>
-    </aside>
-    </motion.section>
+        <aside className="md:w-48 w-80 max-w-full  p-3   bg-white md:rounded-none rounded-lg md:h-28 py-2 md:flex-col flex-row flex gap-2 md:gap-0">
+          <TabItem
+            title={"Request Type"}
+            index={0}
+            activeIndex={activeIndex}
+            onclick={() => {
+              setActiveIndex(0);
+            }}
+          />
+          <TabItem
+            title={"Status Types"}
+            index={1}
+            activeIndex={activeIndex}
+            onclick={() => {
+              setActiveIndex(1);
+            }}
+          />
+          <TabItem
+            title={"Alerts"}
+            index={2}
+            activeIndex={activeIndex}
+            onclick={() => {
+              setActiveIndex(2);
+            }}
+          />
+        </aside>
+        <TabContent activeIndex={activeIndex} index={0}>
+          <TicketRequestType />
+        </TabContent>
+        <TabContent activeIndex={activeIndex} index={1}>
+          <TicketStatusTypes />
+        </TabContent>
+        <TabContent activeIndex={activeIndex} index={2}>
+          <TicketAlerts />
+        </TabContent>
+      </motion.section>
     </AnimatePresence>
-    </TabsContent>
-  )
-}
+  );
+};
+
+const TabItem = ({ title, onclick, index, activeIndex }) => {
+  return (
+    <div
+      className={`relative h-8 w-full grid place-items-center text-[0.75rem] md:text-[0.9rem] font-semibold text- hover:cursor-pointer ${
+        index === activeIndex &&
+        "bg-neutral-200 border-b-2 transition-all duration-500 rounded-md border-b-blue-400"
+      }`}
+      onClick={onclick}
+    >
+      <span className={"absolute  font-semibold"}>{title}</span>
+    </div>
+  );
+};
+
+const TabContent = ({ index, children, activeIndex }) => {
+  if (activeIndex !== index) return null;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="w-full flex-1 "
+    >
+      {children}
+    </motion.section>
+  );
+};
