6a55e879fc52e865bc83c6f44351e3742bcb9e24
ehiosu <ehiandbrothers@gmail.com>
2024-02-02 11:34:15 +0100

    doc formatting

--- datr/src/CPD/Layout/CPDLayout.jsx
+++ datr/src/CPD/Layout/CPDLayout.jsx
@@ -3,9 +3,9 @@ import { Sidebar } from "../Sidebar/Sidebar";
 import { Outlet } from "react-router";
 export const CPDLayout = () => {
   return (
-    <main className="flex w-full bg-my-gray max-w-screen-2xl ">
+    <main className="flex w-full bg-my-gray max-w-screen-2xl overflow-y-hidden">
       <Sidebar />
-      <div className="xl:w-[75%] lg:w-[85%] w-full mx-auto  overflow-x-hidden">
+      <div className="xl:w-[75%] lg:w-[85%] w-full mx-auto min-h-screen overflow-y-auto overflow-x-hidden">
         <Outlet />
       </div>
     </main>
