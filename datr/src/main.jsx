363278b01dbc80ab40f4c3bdbacdccb3e8c7a152
ehiosu <ehiandbrothers@gmail.com>
2024-02-23 11:11:20 +0100

    add toaster root add axios context provider component

--- datr/src/main.jsx
+++ datr/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AxiosClient } from "./api/useAxiosClient.jsx";
+import { Toaster } from "./components/ui/toaster.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AxiosClient>
+      <App />
+    </AxiosClient>
+    <Toaster />
+  </React.StrictMode>
+);
