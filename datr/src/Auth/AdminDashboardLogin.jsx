195e47060c9f541564bd27d1632a550d316e9a7d
ehiosu <ehiandbrothers@gmail.com>
2024-03-07 09:28:32 +0100

    fix styling for absolute images

--- datr/src/Auth/AdminDashboardLogin.jsx
+++ datr/src/Auth/AdminDashboardLogin.jsx
@@ -7,14 +7,19 @@ import { useNavigate } from "react-router";
 export const AdminDashboardLogin = () => {
   const Navigate = useNavigate();
   return (
-    <main className="md:max-h-[100vh] overflow-hidden  overfloverflow-x-hidden max-w-[100vw] md:overflow-y-hidden h-auto  text-center">
+    <main className="md:max-h-screen min-h-screen overflow-hidden  overfloverflow-x-hidden max-w-[100vw] md:overflow-y-hidden h-auto  text-center relative">
+      <img
+        src={logo}
+        alt=""
+        className="absolute  opacity-30 left-0 -translate-x-1/3 w-[140px] md:w-[440px] top-0 -translate-y-1/4 z-[-1]"
+      />
       <section className="grid grid-cols-4 lg:h-[25vh] max-w-full ">
         <div className="logo-img col-span-1 ">
-          <img
+          {/* <img
             src={logo}
             alt=""
             className="absolute  opacity-30 md:top-[-5%] md:right-[77%] object-fit md:w-[460px] md:h-[460px] w-[250px] h-[250px] right-[80%] top-[-10%] "
-          />
+          /> */}
         </div>
         <div className="md:col-span-3 col-span-4  md:mt-10">
           <div className="flex w-full    items-center md:justify-between justify-center gap-4 md:gap-0">
@@ -41,13 +46,18 @@ export const AdminDashboardLogin = () => {
       <div className="lg:w-[80vw] w-full mx-auto lg:h-[45vh] h-auto p-2">
         <AuthForm />
       </div>
-      <div className="h-0 lg:h-auto">
+      {/* <div className="h-0 lg:h-auto">
         <img
           src={logo}
           alt=""
           className="md:ml-[85vw] md:h-[530px] md:w-[530px] relative z-[-1] -translate-y-[40%] ml-[60%]  object-contain lg:-translate-y-48  opacity-30"
         />
-      </div>
+      </div> */}
+      <img
+        src={logo}
+        alt=""
+        className="absolute w-[320px] right-0 translate-x-1/2 bottom-0  opacity-30 z-[-1]"
+      />
     </main>
   );
 };
