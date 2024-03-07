ee9d7db3d4f6c50a593a8cb9af889790939d7707
ehiosu <ehiandbrothers@gmail.com>
2024-03-07 09:50:22 +0100

    update sidebar content to work based on user role

--- datr/src/CPD/Sidebar/Content.jsx
+++ datr/src/CPD/Sidebar/Content.jsx
@@ -5,7 +5,45 @@ import { TbReportSearch } from "react-icons/tb";
 import { FiSettings } from "react-icons/fi";
 import { BsPersonPlus } from "react-icons/bs";
 import { BsClipboard2Data } from "react-icons/bs";
-export const content = [
+export const cpoSidebarContent = [
+  {
+    Name: "Dashboard",
+    Icon: <AiOutlineStock />,
+    SubMenu: [],
+    to: "/CPD/Dashboard",
+  },
+  {
+    Name: "Tickets",
+    Icon: <HiOutlineTicket />,
+    SubMenu: [
+      { Name: "Open", Info: 4, to: "/CPD/Tickets/Open" },
+      { Name: "Resolved", Info: 0, to: "/CPD/Tickets/Resolved" },
+      { Name: "Unresolved", Info: 0, to: "/CPD/Tickets/Unresolved" },
+      { Name: "Unassigned", Info: 0, to: "/CPD/Tickets/Unassigned" },
+      { Name: "Escalated", Info: 2, to: "/CPD/Tickets/Escalated" },
+      { Name: "All", Info: 0, to: "/CPD/Tickets/All" },
+    ],
+    to: "/CPD/Tickets/All",
+  },
+  {
+    Name: "Messages",
+    Icon: <BiMessageSquareDetail />,
+    SubMenu: [
+      { Name: "Inbox", Info: 4, to: "/CPD/Messages?location=inbox" },
+      { Name: "Sent Items", Info: 0, to: "/CPD/Messages?location=sent" },
+      { Name: "Draft", Info: 0, to: "/CPD/Messages?location=drafts" },
+    ],
+    to: "/CPD/Messages?location=inbox",
+  },
+  {
+    Name: "Help",
+    Icon: <BsPersonPlus />,
+    SubMenu: [],
+    to: "/Help",
+  },
+];
+
+export const adminSidebarContent = [
   {
     Name: "Dashboard",
     Icon: <AiOutlineStock />,
