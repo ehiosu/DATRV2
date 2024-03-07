b0a2c5c987bd9bc8e1de848583ca4f357e63e571
ehiosu <ehiandbrothers@gmail.com>
2024-03-07 09:38:28 +0100

    add functionality to get live data

--- datr/src/CPD/Pages/EscalatedTickets.jsx
+++ datr/src/CPD/Pages/EscalatedTickets.jsx
@@ -9,8 +9,20 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { EscalatedTicketsTable } from "../Components/DataTable";
+import { useAxiosClient } from "../../api/useAxiosClient";
+import { useQuery } from "@tanstack/react-query";
 
 export const EscalatedTickets = () => {
+  const { axios } = useAxiosClient();
+  const ticketsQuery = useQuery({
+    queryKey: ["tickets", "escalated"],
+    queryFn: () =>
+      axios("tickets/status?value=ESCALATED", {
+        method: "GET",
+      })
+        .then((resp) => resp.data)
+        .catch((err) => err),
+  });
   return (
     <section className="w-full">
       <SearchPage heading={"Escalated Tickets"}>
@@ -27,7 +39,19 @@ export const EscalatedTickets = () => {
           </div>
         </div>
         <div className="w-full    max-h-[75vh]  mt-4 overflow-y-auto ">
-          <EscalatedTicketsTable />
+          {ticketsQuery.isSuccess && (
+            <EscalatedTicketsTable data={ticketsQuery.data.tickets} />
+          )}
+          {ticketsQuery.isError && (
+            <div className="w-full h-full grid place-items-center  min-h-[60vh]">
+              <p className="Font-semibold text-[2rem] text-neutral-500">
+                {ticketsQuery.error.message ===
+                "Request failed with status code 401"
+                  ? "You don't have access to this resource!"
+                  : ticketsQuery.error.message}
+              </p>
+            </div>
+          )}
         </div>
       </SearchPage>
     </section>
