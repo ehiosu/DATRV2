b261adc3cb1c382b40a68f49700cbfa4ecb602da
ehiosu <ehiandbrothers@gmail.com>
2024-03-07 09:27:10 +0100

    delete useAuth file,replace with typescript file

--- datr/src/api/useAuth.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { useState } from 'react'
-import { useAuthStore } from '../store/AuthStore'
-
-export const useAuth = () => {
-  return useAuthStore((state)=>({access:state.access,refresh:state.refresh,updateTokens:state.updateTokens,updateUser:state.updateUser,user:state.user,generalUpdate:state.generalUpdate}))
-}
