8b4558f18814ea776a46b237d074f7403af5799a
ehiosu <ehiandbrothers@gmail.com>
2024-03-07 09:36:36 +0100

    add tickets pulling and filtering functionality to all tickets page

--- datr/src/CPD/Pages/AllTickets.jsx
+++ datr/src/CPD/Pages/AllTickets.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SearchPage } from "../../Reusable/SearchPage";
 import {
   DropdownMenu,
@@ -6,11 +6,100 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { AiOutlineArrowDown } from "react-icons/ai";
+} from "../../components/ui/dropdown-menu";
+import { AiOutlineArrowDown, AiOutlineClose } from "react-icons/ai";
 import { GeneralTicketsTable } from "../Components/DataTable";
-
+import { useQuery as useLocalQuery } from "../Sidebar/Hooks/useQuery";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../../components/ui/popover";
+import { useAxiosClient } from "../../api/useAxiosClient";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { useAuth } from "../../api/useAuth";
 export const AllTickets = () => {
+  const cpo = useLocalQuery().get("cpo");
+  const status = useLocalQuery().get("status");
+  const assignee = useLocalQuery().get("assignee");
+  const { axios } = useAxiosClient();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(1);
+  const nav = useNavigate();
+  const ticketsQuery = useQuery({
+    queryKey: ["tickets", "all", `${currentPage}`],
+    retry: false,
+    staleTime: Infinity,
+    queryFn: () => {
+      if (!cpo && !status && !assignee) {
+        return axios("tickets/all?page=0&size=10", {
+          method: "GET",
+        }).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+      if (cpo && !status && !assignee) {
+        return axios(
+          `/tickets/cpo?email=${cpo}&page=${currentPage - 1}&size=10`
+        ).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+      if (!cpo && status && !assignee) {
+        return axios(
+          `/tickets/status?value=${status}&page=${currentPage - 1}&size=10`
+        ).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+      if (!cpo && !status && assignee) {
+        return axios(
+          `/tickets/assignee?email=${assignee}&page=${currentPage - 1}&size=10`
+        ).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+      if (cpo && status && !assignee) {
+        return axios(
+          `/tickets/cpo-status?email=${cpo}&status=${status}&page=${
+            currentPage - 1
+          }&size=10`
+        ).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+      if (!cpo && assignee && status) {
+        return axios(
+          `/tickets/assignee?email=${assignee}&status=${status}&page=${
+            currentPage - 1
+          }&size=10`
+        ).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      } else {
+        return axios("tickets/all?page=0&size=10", {
+          method: "GET",
+        }).then((resp) => {
+          setMaxPages(() => resp.data.totalPages);
+          return resp.data;
+        });
+      }
+    },
+  });
+
+  useEffect(() => {
+    console.log({ cpo, status, assignee });
+    ticketsQuery.refetch();
+  }, [cpo, assignee, status]);
   return (
     <section className="w-full">
       <SearchPage heading={"All Tickets"}>
@@ -22,12 +111,93 @@ export const AllTickets = () => {
             </button>
           </div>
           <div className="md:ml-auto flex    gap-3  flex-wrap md:my-0 my-2">
-            <FilterButton />
+            <FilterButton assignee={assignee} cpo={cpo} status={status} />
             <RangeSelectButton />
           </div>
         </div>
-        <div className="w-full  flex-1  ">
-          <GeneralTicketsTable />
+        <div className="flex items-center md:justify-end flex-wrap md:space-x-4">
+          {cpo && (
+            <div className="flex flex-col  bg-white rounded-md py-1 px-3 space-y-0 ">
+              <div className="flex justify-between items-center text-[0.8rem] my-0  space-x-2">
+                <p className="font-semibold"> {cpo}</p>
+                <Button
+                  className="p-1 w-4 h-4"
+                  onClick={() => {
+                    nav(
+                      `/CPD/Tickets/All?cpo=${""}&status=${status}&assignee=${assignee}`
+                    );
+                  }}
+                  variant={"ghost"}
+                >
+                  <AiOutlineClose />
+                </Button>
+              </div>
+              <span className="font-semibold text-xs text-neutral-400">
+                Active CPO:
+              </span>
+            </div>
+          )}
+
+          {status && (
+            <div className="flex flex-col  bg-white rounded-md py-1 px-3 space-y-0 ">
+              <div className="flex justify-between items-center text-[0.8rem] my-0  space-x-2">
+                <p className="font-semibold"> {status}</p>
+                <Button
+                  className="p-1 w-4 h-4"
+                  onClick={() => {
+                    nav(
+                      `/CPD/Tickets/All?cpo=${cpo}&status=${""}&assignee=${assignee}`
+                    );
+                  }}
+                  variant={"ghost"}
+                >
+                  <AiOutlineClose />
+                </Button>
+              </div>
+              <span className="font-semibold text-xs text-neutral-400">
+                Active Status:
+              </span>
+            </div>
+          )}
+
+          {assignee && (
+            <div className="flex flex-col  bg-white rounded-md py-1 px-3 space-y-0 ">
+              <div className="flex justify-between items-center text-[0.8rem] my-0  space-x-2">
+                <p className="font-semibold"> {assignee}</p>
+                <Button
+                  className="p-1 w-4 h-4"
+                  onClick={() => {
+                    nav(
+                      `/CPD/Tickets/All?cpo=${cpo}&status=${status}&assignee=${""}`
+                    );
+                  }}
+                  variant={"ghost"}
+                >
+                  <AiOutlineClose />
+                </Button>
+              </div>
+              <span className="font-semibold text-xs text-neutral-400">
+                Active Assignee:
+              </span>
+            </div>
+          )}
+        </div>
+        <div className="w-full  flex-1 ">
+          {ticketsQuery.isSuccess && (
+            <GeneralTicketsTable
+              generalTicketData={ticketsQuery.data.tickets}
+            />
+          )}
+          {ticketsQuery.isError && (
+            <div className="w-full h-full grid place-items-center  min-h-[60vh]">
+              <p className="Font-semibold text-[2rem] text-neutral-500">
+                {ticketsQuery.error.message ===
+                "Request failed with status code 401"
+                  ? "You don't have access to this resource!"
+                  : " "}
+              </p>
+            </div>
+          )}
         </div>
       </SearchPage>
     </section>
@@ -57,14 +227,226 @@ const ViewChangeBtn = () => {
   );
 };
 
-const FilterButton = () => {
+const FilterButton = ({ assignee, status, cpo }) => {
+  const { axios } = useAxiosClient();
+  const [searchedName, setSearchedName] = useState("");
+  const { user } = useAuth();
+  const statusQuery = useQuery({
+    queryKey: ["Tickets", "statuses"],
+    queryFn: () =>
+      axios("tickets/statuses", {
+        method: "GET",
+      }).then((resp) => {
+        console.log(resp.data);
+        return resp.data;
+      }),
+    staleTime: Infinity,
+  });
+  const cposQuery = useQuery({
+    queryKey: ["cpos"],
+    retry: false,
+    queryFn: () =>
+      axios("/cpo/all", {
+        method: "GET",
+      }).then((resp) => resp.data),
+    staleTime: Infinity,
+  });
+  const nav = useNavigate();
+  const navTo = (_email = cpo, _assignee = assignee, _status = status) => {
+    nav(
+      `/CPD/Tickets/All?cpo=${_email || ""}&assignee=${
+        _assignee || ""
+      }&status=${_status || ""}`,
+      {
+        replace: true,
+      }
+    );
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="bg-[#EEEEEE] flex justify-center items-center  w-32   text-[0.8275rem]  outline-none h-10    rounded-md  shadow-md  gap-3   font-semibold   ">
-        Filter by All <AiOutlineArrowDown />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className=" bg-white w-28   rounded-md     z-50 my-2"></DropdownMenuContent>
-    </DropdownMenu>
+    <Popover>
+      <PopoverTrigger
+        className="w-max px-4 flex flex-row gap-2 items-center bg-white rounded-lg border-2 border-neutral-100 shadow-sm text-[0.8275rem]"
+        disabled={!user.roles.includes("CPO") && !user.roles.includes("ADMIN")}
+      >
+        Filter <AiOutlineArrowDown />
+      </PopoverTrigger>
+      <PopoverContent
+        className="w-40 flexflex-col items-center dark:w-40 px-2 h-max dark:h-max py-2"
+        side="bottom"
+      >
+        <Popover>
+          <PopoverTrigger className="text-[0.85rem] w-full hover:bg-blue-100 p-1 grid  place-items-center cursor-pointer hover:border-b-2  hover:font-semibold hover:text-neutral-600 hover:border-b-purple-300 rounded-md">
+            Filter by Creator
+          </PopoverTrigger>
+          {true && (
+            <PopoverContent className="px-2 py-2 pb-6" side="left">
+              {cpo && (
+                <div className="flex items-center">
+                  <p className="text-[0.8275rem] flex flex-col whitespace-nowrap ">
+                    Active CPO: <span className="font-semibold">{cpo}</span>
+                  </p>
+                  <Button
+                    onClick={() => {
+                      navTo("", assignee, status);
+                    }}
+                    className="w-4 grid place-items-center aspect-square ml-auto"
+                    variant={"ghost"}
+                  >
+                    <AiOutlineClose />
+                  </Button>
+                </div>
+              )}
+              <Input
+                placeholder="Search CPO email"
+                className="text-[0.8275rem]  dark:bg-white dark:focus:bg-neutral-200 dark:border-2 dark:border-neutral-500 transition-colors my-5 h-8 text-neutral-400 placeholder:text-neutral-600"
+                onChange={(e) => {
+                  setSearchedName(e.target.value);
+                }}
+              />
+              <ul>
+                {cposQuery.data
+                  ?.filter((_cpo) =>
+                    _cpo.ncaaUserEmail.startsWith(searchedName)
+                  )
+                  .map((_cpo, index) => (
+                    <li
+                      onClick={() => {
+                        if (_cpo === cpo) return;
+                        navTo(_cpo.ncaaUserEmail);
+                      }}
+                      aria-disabled={cpo === _cpo.ncaaUserEmail}
+                      className="text-[0.8275rem] my-2 h-8 grid place-items-center rounded-md  aria-disabled:bg-neutral-400 aria-disabled:hover:cursor-not-allowed text-center hover:cursor-pointer hover:border-b-2 hover:border-b-blue-400 hover:py-1 transition-all font-semibold text-neutral-500"
+                      key={_cpo.id}
+                    >
+                      {_cpo.ncaaUserEmail}
+                    </li>
+                  ))}
+              </ul>
+            </PopoverContent>
+          )}
+        </Popover>
+        <Popover>
+          <PopoverTrigger className="text-[0.85rem] w-full hover:bg-blue-100 p-1 grid  place-items-center cursor-pointer hover:border-b-2  hover:font-semibold hover:text-neutral-600 hover:border-b-purple-300 rounded-md">
+            Filter by Status
+          </PopoverTrigger>
+          {cposQuery.isSuccess && (
+            <PopoverContent className="px-2 " side="left">
+              {status && (
+                <div className="flex items-center">
+                  <p className="text-[0.8275rem] flex flex-col whitespace-nowrap ">
+                    Active Status:{" "}
+                    <span className="font-semibold">{status}</span>
+                  </p>
+                  <Button
+                    onClick={() => {
+                      navTo(cpo, assignee, "");
+                    }}
+                    className="w-4 grid place-items-center aspect-square ml-auto"
+                    variant={"ghost"}
+                  >
+                    <AiOutlineClose />
+                  </Button>
+                </div>
+              )}
+              <Input
+                placeholder="Search for a status"
+                className="text-[0.8275rem]  dark:bg-white dark:focus:bg-neutral-200 dark:border-2 dark:border-neutral-500 transition-colors my-5 h-8 text-neutral-400 placeholder:text-neutral-600"
+                onChange={(e) => {
+                  setSearchedName(e.target.value);
+                }}
+              />
+              <ul>
+                {statusQuery.isSuccess &&
+                  statusQuery.data
+                    ?.filter((_status) =>
+                      _status
+                        .toLowerCase()
+                        .startsWith(searchedName.toLowerCase())
+                    )
+                    .map((_status, index) => (
+                      <li
+                        onClick={() => {
+                          if (_status === status) return;
+                          navTo(cpo, assignee, _status);
+                        }}
+                        aria-disabled={_status === status}
+                        className="text-[0.8275rem] h-8 grid place-items-center my-2 aria-disabled:text-white aria-disabled:font-semibold  aria-disabled:bg-neutral-400 aria-disabled:hover:cursor-not-allowed text-center hover:cursor-pointer hover:border-b-2 hover:border-b-blue-400 hover:py-1 transition-all font-semibold text-neutral-500"
+                        key={_status}
+                      >
+                        {_status}
+                      </li>
+                    ))}
+              </ul>
+            </PopoverContent>
+          )}
+        </Popover>
+
+        <Popover>
+          <PopoverTrigger className="text-[0.85rem] w-full hover:bg-blue-100 p-1 grid  place-items-center cursor-pointer hover:border-b-2  hover:font-semibold hover:text-neutral-600 hover:border-b-purple-300 rounded-md">
+            Filter by Assignee
+          </PopoverTrigger>
+          {cposQuery.isSuccess && (
+            <PopoverContent className="px-2 py-2 pb-6" side="left">
+              {assignee && (
+                <div className="flex items-center">
+                  <p className="text-[0.8275rem] flex flex-col whitespace-nowrap ">
+                    Active Assignee:{" "}
+                    <span className="font-semibold">{assignee}</span>
+                  </p>
+                  <Button
+                    onClick={() => {
+                      navTo(cpo, "", status);
+                    }}
+                    className="w-4 grid place-items-center aspect-square ml-auto"
+                    variant={"ghost"}
+                  >
+                    <AiOutlineClose />
+                  </Button>
+                </div>
+              )}
+              <Input
+                placeholder="Search for a status"
+                className="text-[0.8275rem]  dark:bg-white dark:focus:bg-neutral-200 dark:border-2 dark:border-neutral-500 transition-colors my-5 h-8 text-neutral-400 placeholder:text-neutral-600"
+                onChange={(e) => {
+                  setSearchedName(e.target.value);
+                }}
+              />
+              <ul>
+                {cposQuery.data
+                  ?.filter((_cpo) =>
+                    _cpo.ncaaUserEmail
+                      .toLowerCase()
+                      .startsWith(searchedName.toLowerCase())
+                  )
+                  .map((_cpo, index) => (
+                    <li
+                      onClick={() => {
+                        if (_cpo === assignee) return;
+                        navTo(cpo, _cpo.ncaaUserEmail, status);
+                      }}
+                      aria-disabled={_cpo === assignee}
+                      className="text-[0.8275rem] my-2 aria-disabled:text-white aria-disabled:font-semibold  aria-disabled:bg-neutral-400 aria-disabled:hover:cursor-not-allowed text-center hover:cursor-pointer hover:border-b-2 hover:border-b-blue-400 hover:py-1 transition-all font-semibold text-neutral-500"
+                      key={_cpo.ncaaUserEmail}
+                    >
+                      {_cpo.ncaaUserEmail}
+                    </li>
+                  ))}
+              </ul>
+            </PopoverContent>
+          )}
+        </Popover>
+        <Button
+          className="w-full dark:py-1 py-1 h-8 my-1"
+          onClick={() => {
+            nav("/CPD/Tickets/All");
+          }}
+          variant={"ghost"}
+        >
+          Clear filters
+        </Button>
+      </PopoverContent>
+    </Popover>
   );
 };
 const RangeSelectButton = () => {
