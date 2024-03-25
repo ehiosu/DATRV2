288b22c17bce2243c8f0e7cf21177edfcf793277
ehiosu <ehiandbrothers@gmail.com>
2024-03-25 12:46:51 +0100

    add sonner toaster component to application root

--- datr/src/main.jsx
+++ datr/src/main.jsx
@@ -4,12 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { AxiosClient } from "./api/useAxiosClient.jsx";
 import { Toaster } from "./components/ui/toaster.tsx";
-
+import { Toaster as SonnerToaster } from "sonner";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AxiosClient>
       <App />
     </AxiosClient>
+    <SonnerToaster />
     <Toaster />
   </React.StrictMode>
 );
