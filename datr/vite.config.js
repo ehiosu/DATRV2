d09e45c7932952ae8864d5a9b25ac1c8d3286f1f
ehiosu <ehiandbrothers@gmail.com>
2024-03-25 12:45:42 +0100

    updated vite config for production

--- datr/vite.config.js
+++ datr/vite.config.js
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+   },
+  preview:{
+    host:true,
+    strictPort:true,
+    port:8080
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,4 +24,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
   },
+  
 })
