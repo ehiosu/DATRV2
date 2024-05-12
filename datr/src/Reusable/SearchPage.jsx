f1de5242ac609c9e2300381c0e8e66b8475422e5
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 23:27:12 +0100

    update search page

--- datr/src/Reusable/SearchPage.jsx
+++ datr/src/Reusable/SearchPage.jsx
@@ -13,55 +13,80 @@ import { useNavigate } from "react-router";
 import { Button } from "../components/ui/button";
 import { useAuth } from "../api/useAuth";
 import { User } from "lucide-react";
-export const SearchPage = ({ children, heading, isRedirect = false }) => {
+export const SearchPage = ({
+  children,
+  heading,
+  isRedirect = false,
+  SearchElement,
+}) => {
   const [searching, setSearching] = useState(false);
-
+  const { user } = useAuth();
+  const isAirline = user.roles.includes("AIRLINE");
   const nav = useNavigate();
+
   return (
     <div className="w-full   flex    flex-col   gap-2  p-2">
-      <div className="flex flex-wrap  justify-start md:justify-between items-center w-full ">
-        <p
-          className={`text-[1.2rem] ${
-            isRedirect && "hover:cursor-pointer"
-          }  font-semibold`}
-          onClick={() => {
-            if (isRedirect) {
-              nav(-1);
-            }
-          }}
-        >
-          {heading}
-        </p>
-        <div className="flex items-center mt-2 gap-2 text-[1.2rem] font-extrabold mx-auto">
-          <CiBookmarkCheck className="hover:text-blue-700" />
-          <CiBellOn />
-          <CiTrash className="hover:text-red-700  " />
-        </div>
-        <div className={`   h-16  flex   items-center  justify-end gap-3 `}>
-          <div
-            className={`${
-              searching ? "lg:w-[35rem]  w-[22rem]" : "w-60"
-            } transition-all   h-12    ro   relative grid place-items-center`}
+      <div className="flex flex-wrap  justify-between items-center w-full md:space-x-2">
+        {!isAirline ? (
+          <p
+            className={`text-[1.2rem] ${
+              isRedirect && "hover:cursor-pointer"
+            }  font-semibold`}
+            onClick={() => {
+              if (isRedirect) {
+                nav(-1);
+              }
+            }}
           >
-            <input
-              type="text"
-              name=""
-              className="w-full   h-8    border-2 border-gray-400   rounded-xl  px-2  py-2  text-[0.8275rem] text-neutral-600   outline-none   "
-              id=""
-              placeholder="Search"
-              onFocus={() => {
-                setSearching(() => true);
+            {heading}
+          </p>
+        ) : (
+          <div className="flex flex-col space-y-1 ">
+            <p className="font-semibold text-lg">Welcome, {user.airline} </p>
+            <p
+              className={`text-[1.2rem] ${
+                isRedirect && "hover:cursor-pointer"
+              }  font-semibold`}
+              onClick={() => {
+                if (isRedirect) {
+                  nav(-1);
+                }
               }}
-              onBlur={() => {
-                setSearching(() => false);
-              }}
-            />
-            <div
-              className={`absolute  top-12 right-0  ${
-                searching ? "block" : "hidden"
-              } h-40 w-full  shadow-md  z-[10] absolute bg-white`}
-            ></div>
+            >
+              {heading}
+            </p>
           </div>
+        )}
+
+        <div className={`   h-16  flex  items-start  justify-end space-x-2 `}>
+          {SearchElement ? (
+            <SearchElement />
+          ) : (
+            <div
+              className={`${
+                searching ? "lg:w-[35rem]  w-[22rem]" : "w-60"
+              } transition-all   h-12   relative grid place-items-center`}
+            >
+              <input
+                type="text"
+                name=""
+                className="w-full   h-8    border-2 border-gray-400   rounded-xl  px-2  py-2  text-[0.8275rem] text-neutral-600   outline-none   "
+                id=""
+                placeholder="Search"
+                onFocus={() => {
+                  setSearching(() => true);
+                }}
+                onBlur={() => {
+                  setSearching(() => false);
+                }}
+              />
+              <div
+                className={`absolute  top-12 right-0  ${
+                  searching ? "block" : "hidden"
+                } h-40 w-full  shadow-md  z-[10] absolute bg-white`}
+              ></div>
+            </div>
+          )}
           <UserHeader searching={searching} />
         </div>
       </div>
@@ -76,7 +101,9 @@ const UserHeader = ({ searching }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
-        className={`outline-none ${searching ? "hidden" : "block"} ml-auto`}
+        className={`outline-none ${
+          searching ? "hidden" : "block"
+        } ml-auto mt-1.5`}
       >
         <Avatar className="w-8  aspect-square rounded-full  border-2  border-gray-400/40  grid  place-items-center overflow-hidden">
           <AvatarImage
