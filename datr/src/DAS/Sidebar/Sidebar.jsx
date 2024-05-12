f2f0f24896ef2aa3bb15141d7ca57b630d46817a
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 23:25:31 +0100

    update das sidebar to cater for searching

--- datr/src/DAS/Sidebar/Sidebar.jsx
+++ datr/src/DAS/Sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { content } from "./Content";
 import { AiOutlineClose } from "react-icons/ai";
-import { useLocation, useNavigate } from "react-router";
+import { useLocation, useNavigate, useParams } from "react-router";
 import {
   Tooltip,
   TooltipProvider,
@@ -23,6 +23,11 @@ import {
   PopoverTrigger,
 } from "../../components/ui/popover";
 import { BsThreeDots } from "react-icons/bs";
+import { MdCancel, MdDashboard } from "react-icons/md";
+import { Search, TimerOff } from "lucide-react";
+import { TbMapPinCancel } from "react-icons/tb";
+import { useAirlineContext } from "../Layout/DASLayout";
+
 export const Sidebar = () => {
   return (
     <>
@@ -35,10 +40,11 @@ export const Sidebar = () => {
 const MidSizedSidebar = () => {
   const [open, setOpen] = useState(false);
   const nav = useNavigate();
+  const { Location } = useParams();
   return (
     <aside
       className={`md:block hidden max-h-[97vh] h-[97vh] overflow-y-auto sticky top-[1.5vh] ${
-        open ? "md:w-[22vw] lg:w-[14vw]" : "w-12"
+        open ? "md:w-[26vw] lg:w-[14vw]" : "w-12"
       } transition-all mx-2 my-2 bg-white rounded-md shadow-md`}
     >
       {!open && (
@@ -48,10 +54,34 @@ const MidSizedSidebar = () => {
               setOpen(true);
             }}
           />
-
+          <SearchItem open={open} />
           {content.map((item) => (
             <SidebarItem {...item} nav={nav} open={open} />
           ))}
+          <SidebarItem
+            open={open}
+            Name={"Dashboard"}
+            nav={nav}
+            Icon={<MdDashboard className="text-black" />}
+            SubMenu={[]}
+            to={`/DAS/${Location}/Dashboard`}
+          />
+          <SidebarItem
+            open={open}
+            Name={"Delays"}
+            nav={nav}
+            Icon={<TimerOff color="black" className="text-black " />}
+            SubMenu={[]}
+            to={`/DAS/${Location}/Delays`}
+          />
+          <SidebarItem
+            open={open}
+            Name={"Cancelled Flights"}
+            nav={nav}
+            Icon={<MdCancel color="black" className="text-black " />}
+            SubMenu={[]}
+            to={`/DAS/${Location}/Cancelled`}
+          />
         </div>
       )}
 
@@ -62,10 +92,55 @@ const MidSizedSidebar = () => {
               setOpen(close);
             }}
           />
-          <img src="/NCAA.png" alt="NCAA Logo" className="my-2 w-[80%]" />
+          <div
+            className=""
+            role="button"
+            onClick={() => {
+              nav("/CPD/Dashboard");
+            }}
+          >
+            <img
+              src="/NCAA.png"
+              alt="NCAA Logo test"
+              className="my-2 w-[80%]"
+            />
+          </div>
+          <SearchItem open={open} />
+
           {content.map((item) => (
             <SidebarItem {...item} nav={nav} open={open} />
           ))}
+          <SidebarItem
+            open={open}
+            Name={"Dashboard"}
+            nav={nav}
+            Icon={
+              <MdDashboard className="text-black group-hover:text-blue-300" />
+            }
+            SubMenu={[]}
+            to={`/DAS/${Location}/Dashboard`}
+          />
+          <SidebarItem
+            open={open}
+            Name={"Delays"}
+            nav={nav}
+            Icon={
+              <TimerOff
+                className="text-black  group-hover:text-blue-300"
+                size={12}
+              />
+            }
+            SubMenu={[]}
+            to={`/DAS/${Location}/Delays`}
+          />
+          <SidebarItem
+            open={open}
+            Name={"Cancelled Flights"}
+            nav={nav}
+            Icon={<MdCancel color="black" className="text-black " />}
+            SubMenu={[]}
+            to={`/DAS/${Location}/Cancelled`}
+          />
         </div>
       )}
     </aside>
@@ -87,10 +162,38 @@ const SmallSideBar = () => {
   );
 };
 
+const SearchItem = ({ open }) => {
+  const dialogFunctions = useAirlineContext();
+  useEffect(() => {
+    const down = (e) => {
+      if (e.ctrlKey && e.key == "s") {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log("pressed");
+        dialogFunctions.toggle();
+      }
+    };
+    document.addEventListener("keydown", down);
+
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+  return (
+    <div
+      role="button"
+      className={`h-7 group flex flex-row items-center space-x-2 ${
+        open ? " w-max pl-2 pr-8" : "justify-center w-7 "
+      } rounded-lg mt-2 bg-neutral-100`}
+      onClick={() => dialogFunctions.setOpen(true)}
+    >
+      <Search className="w-5 h-5 opacity-0 group-hover:opacity-100 transition duration-500 ease-out rounded-md shrink group-hover:bg-neutral-200 p-1" />
+      {open && <p className="text-sm text-neutral-500">Search</p>}
+    </div>
+  );
+};
 const SidebarItem = ({ Name, Icon, SubMenu, open, to, nav }) => {
   return (
     <div
-      className={`flex items-center gap-2 my-3 ${
+      className={`flex items-center gap-2 my-3 group hover:text-blue-300 ${
         open &&
         window.location.pathname !== to &&
         "hover:ring-2 hover:ring-blue-500/40 rounded-md hover:bg-neutral-100 hover:cursor-pointer hover:shadow-md"
