313c0fa8f8d3aea2c13a33a53be9e841e8e60195
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 22:56:46 +0100

    change hover state styling for table row component

--- datr/src/components/ui/table.tsx
+++ datr/src/components/ui/table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-slate-100/50 data-[state=selected]:bg-slate-100 dark:hover:bg-slate-800/50 dark:data-[state=selected]:bg-slate-800",
+      "border-b transition-colors hover:bg-slate-100/50 data-[state=selected]:bg-slate-100 dark:hover:bg-slate-200/50 dark:data-[state=selected]:bg-slate-800 group",
       className
     )}
     {...props}
