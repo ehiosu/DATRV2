b3cc46031240e361195eeb7470ae50c29849fa20
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 22:57:46 +0100

    add dynamism to recent tickets component

--- datr/src/CPD/Components/RecentTickets.jsx
+++ datr/src/CPD/Components/RecentTickets.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { RecentTicketsTable } from "./DataTable";
 import { useNavigate } from "react-router";
+import { useAxiosClient } from "../../api/useAxiosClient";
+import { useAuth } from "../../api/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/components/ui/skeleton.tsx";
 export const RecentTickets = () => {
+  const { axios } = useAxiosClient();
+  const { user } = useAuth();
+  const isAirline = user.roles.includes("AIRLINE");
+
+  const ticketsQuery = useQuery({
+    queryKey: ["tickets", "new"],
+    queryFn: () =>
+      axios(
+        isAirline
+          ? "tickets/airline/status?value=NEW&page=0&size=10"
+          : "tickets/status?value=NEW"
+      ).then((resp) => resp.data.tickets),
+  });
   return (
     <div className="w-full h-full bg-white">
-      <RecentTicketsTable />
+      {ticketsQuery.isSuccess ? (
+        <RecentTicketsTable data={ticketsQuery.data} />
+      ) : (
+        <>
+          <Skeleton className="w-full h-8 my-2" />
+          <Skeleton className="w-full h-8 my-2" />
+          <Skeleton className="w-full h-8 my-2" />
+          <Skeleton className="w-full h-8 my-2" />
+          <Skeleton className="w-full h-8 my-2" />
+        </>
+      )}
     </div>
   );
 };
