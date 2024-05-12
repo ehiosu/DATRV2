a3beac08b20c8f657ec26d8ad2c1012730cf6d7d
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 23:26:03 +0100

    update header component

--- datr/src/Home/Components/Header.jsx
+++ datr/src/Home/Components/Header.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import logo from "/NCAA.png";
 import oip from "/OIP.jpg";
 import { useAuth } from "../../api/useAuth";
-export const Header = () => {
+export const Header = ({ show }) => {
   const { user } = useAuth();
   return (
-    <div className="md:h-[15vh] w-full flex justify-center md:justify-between items-center md:flex-row flex-col h-[30vh]  md:gap-0 gap-2 ">
+    <div className="md:h-[15vh] w-full flex  md:justify-between  items-center md:flex-row flex-col h-[30vh]  md:gap-0 gap-2 ">
       <img
         src={logo}
         alt=""
         className="md:w-52 w-36 md:h-full object-contain"
       />
-      <div className="flex flex-col md:h-full md:w-80 flex-1  justify-center text-center text-[#000066] font-semibold  items-center ">
+      <div className="flex flex-col md:h-full    justify-center text-center text-[#000066] font-semibold  items-center flex-1 outline">
         <p>Welcome</p>
         <p className="text-2xl ">{user.firstName}</p>
       </div>
-      <div className="flex items-center gap-4">
+      {/* <div className="flex items-center gap-4">
         <input
           type="text"
           name=""
@@ -25,7 +25,8 @@ export const Header = () => {
         <div className="w-10 h-10 rounded-full border-2 border-gray-200 gap-2 overflow-hidden">
           <img src={oip} alt="" className="w-full h-full object-contain" />
         </div>
-      </div>
+      </div> */}
+      <div className="h-full aspect-square"></div>
     </div>
   );
 };
