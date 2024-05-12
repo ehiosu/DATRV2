2196db36106af42765fa6234d8ec9ada9328c7e8
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 23:21:36 +0100

    update sidebar styling replace sidebar popover menu with collapsible menu

--- datr/src/CPD/Sidebar/Sidebar.jsx
+++ datr/src/CPD/Sidebar/Sidebar.jsx
@@ -43,8 +43,8 @@ const MidSizedSidebar = ({ content }) => {
   const nav = useNavigate();
   return (
     <aside
-      className={`md:block hidden max-h-[97vh] h-[97vh] overflow-y-auto sticky top-[1.5vh] ${
-        open ? "md:w-[22vw] lg:w-[14vw]" : "w-12"
+      className={`md:block hidden max-h-[97vh] h-[97vh] fixed  overflow-y-auto left-0 z-[10]  top-[1.5vh] ${
+        open ? "md:w-[26vw] lg:w-[12vw]" : "w-12"
       } transition-all mx-2 my-2 bg-white rounded-md shadow-md`}
     >
       {!open && (
@@ -68,7 +68,14 @@ const MidSizedSidebar = ({ content }) => {
               setOpen(close);
             }}
           />
-          <img src="/NCAA.png" alt="NCAA Logo" className="my-2 w-[80%]" />
+          <div
+            role="button"
+            onClick={() => {
+              nav("/CPD/Dashboard");
+            }}
+          >
+            <img src="/NCAA.png" alt="NCAA Logo" className="my-2 w-[80%]" />
+          </div>
           {content.map((item) => (
             <SidebarItem {...item} nav={nav} open={open} />
           ))}
@@ -92,8 +99,14 @@ const SmallSideBar = ({ content }) => {
     </Sheet>
   );
 };
-
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "../../components/ui/collapsible";
+import { ArrowDown } from "lucide-react";
 const SidebarItem = ({ Name, Icon, SubMenu, open, to, nav }) => {
+  const [isCollapsibleOpen, setCollapsibleOpen] = useState(false);
   return (
     <div
       className={`flex items-center gap-2 my-3 ${
@@ -103,6 +116,7 @@ const SidebarItem = ({ Name, Icon, SubMenu, open, to, nav }) => {
       } ${
         open &&
         window.location.pathname === to &&
+        SubMenu.length === 0 &&
         "border-b-4 border-b-purple-300/40 rounded-md bg-neutral-100 border-r-2 shadow-md hover:cursor-pointer border-r-blue-300"
       } relative`}
       onClick={() => {
@@ -129,34 +143,32 @@ const SidebarItem = ({ Name, Icon, SubMenu, open, to, nav }) => {
           </Tooltip>
         </TooltipProvider>
       ) : (
-        <span
-          className={`text-[1.4rem] font-thin   ${
-            !open
-              ? window.location.pathname == to
-                ? "bg-neutral-100 text-blue-600 ring-2"
-                : "hover:ring-blue-400 hover:ring-2 hover:text-blue-600 "
-              : ""
-          }  rounded-md p-2 transition-colors hover:cursor-pointer`}
+        <div
+          className={`flex flex-col flex-1 p-2  ${
+            window.location.pathname == to &&
+            !isCollapsibleOpen &&
+            "border-b-4 border-b-purple-300/40 rounded-md bg-neutral-100 border-r-2 shadow-md hover:cursor-pointer border-r-blue-300 "
+          }`}
         >
-          {Icon}
-        </span>
-      )}
-      {open && (
-        <div className="flex items-center justify-between flex-1 p-2 ">
-          <div className="flex flex-col text-black">
-            <p className="text-[0.8275rem]">{Name}</p>
-          </div>
-          {SubMenu.length > 0 && (
-            <Popover>
-              <PopoverTrigger
-                className="ml-auto"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <BsThreeDots />
-              </PopoverTrigger>
-              <PopoverContent side="right" className="w-36 ml-2 ">
+          {SubMenu.length > 0 ? (
+            <Collapsible onOpenChange={setCollapsibleOpen}>
+              <CollapsibleTrigger className={`flex items-center w-full group `}>
+                {Icon}
+                <div
+                  className="flex flex-col text-black ml-1 hover:text-blue-400"
+                  role="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    nav(to);
+                  }}
+                >
+                  <p className="text-[0.8275rem]">{Name}</p>
+                </div>
+                <div className="ml-auto flex-1 flex flex-row  items-center justify-end">
+                  <ArrowDown className="w-4 shrink h-4 group-data-[state=open]:rotate-180 transition-all duration-500 group-data-[state=open]:text-blue-500" />
+                </div>
+              </CollapsibleTrigger>
+              <CollapsibleContent className="">
                 {SubMenu.map((menu) => {
                   return (
                     <div
@@ -167,15 +179,22 @@ const SidebarItem = ({ Name, Icon, SubMenu, open, to, nav }) => {
                       }}
                       className={`my-2 text-center hover:cursor-pointer hover:text-blue-400 ${
                         window.location.pathname === menu.to &&
-                        "bg-neutral-100 border-b-4 rounded-md border-b-purple-200/70"
+                        "bg-neutral-100 border-b-4 rounded-md border-b-purple-200/70 shadow-sm "
                       }`}
                     >
                       <p className="text-[0.8275rem]">{menu.Name}</p>
                     </div>
                   );
                 })}
-              </PopoverContent>
-            </Popover>
+              </CollapsibleContent>
+            </Collapsible>
+          ) : (
+            <div className="flex flex-row items-center">
+              {Icon}
+              <div className="flex flex-col text-black ml-1 mr-auto">
+                <p className="text-[0.8275rem]">{Name}</p>
+              </div>
+            </div>
           )}
         </div>
       )}
