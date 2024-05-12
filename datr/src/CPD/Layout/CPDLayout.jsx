449f39191c7c014087892fd5517dd9964fdb03b2
ehiosu <ehiandbrothers@gmail.com>
2024-05-12 23:00:14 +0100

    update styling for cpd layout

--- datr/src/CPD/Layout/CPDLayout.jsx
+++ datr/src/CPD/Layout/CPDLayout.jsx
@@ -13,7 +13,7 @@ export const CPDLayout = () => {
   return (
     <main className="flex w-full bg-my-gray max-w-screen-2xl overflow-y-hidden">
       <Sidebar />
-      <div className="xl:w-[75%] lg:w-[85%] w-full mx-auto min-h-screen overflow-y-auto overflow-x-hidden">
+      <div className="xl:w-[75%] lg:w-[80%] md:w-[80%]  w-full mx-auto min-h-screen">
         <Outlet />
       </div>
     </main>
