a03af5ffea589b5a2029a3f1c0e5cf504222d4d9
ehiosu <ehiandbrothers@gmail.com>
2024-06-05 02:22:56 +0100

    add placeholder airline entry

---
+++ datr/src/data.ts
@@ -0,0 +1,14 @@
+export const airlines = [
+    "Air Peace",
+    "Arik Air",
+    "Dana Air",
+    "Aero Contractors",
+    "Azman Air",
+    "Max Air",
+    "Ibom Air",
+    "Green Africa Airways",
+    "United Nigeria Airlines",
+    "Overland Airways",
+    "Med-View Airline"
+  ];
+  
