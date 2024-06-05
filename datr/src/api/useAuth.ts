dcab436785f1877ae15837fc08ae93f0054ace66
ehiosu <ehiandbrothers@gmail.com>
2024-06-05 02:23:34 +0100

    update use auth hook

--- datr/src/api/useAuth.ts
+++ datr/src/api/useAuth.ts
@@ -3,5 +3,5 @@ import { useState } from 'react'
 import { useAuthStore } from '../store/AuthStore'
 
 export const useAuth = () => {
-  return useAuthStore((state)=>({access:state.access,refresh:state.refresh,updateTokens:state.updateTokens,updateUser:state.updateUser,user:state.user,generalUpdate:state.generalUpdate,updateVerified:state.updateVerified,updateImageUrl:state.updateImage}))
+  return useAuthStore((state)=>state)
 }
