28cea0adef9a2e797c03af556922fbe61237dcdd
ehiosu <ehiandbrothers@gmail.com>
2024-06-05 02:34:57 +0100

    update sidebar content

--- datr/src/CPD/Sidebar/Content.jsx
+++ datr/src/CPD/Sidebar/Content.jsx
@@ -95,6 +95,49 @@ export const adminSidebarContent = [
     Name: "Data & Statistics",
     Icon: <BsClipboard2Data />,
     SubMenu: [],
-    to: "/CPD/DAS",
+    to: "/DAS/Dashboard",
+  },
+];
+export const DGContent = [
+  {
+    Name: "Dashboard",
+    Icon: <AiOutlineStock />,
+    SubMenu: [],
+    to: "/CPD/Dashboard",
+  },
+  {
+    Name: "Tickets",
+    Icon: <HiOutlineTicket />,
+    SubMenu: [
+      { Name: "Open", Info: 4, to: "/CPD/Tickets/Open" },
+      { Name: "Resolved", Info: 0, to: "/CPD/Tickets/Resolved" },
+      { Name: "Unresolved", Info: 0, to: "/CPD/Tickets/Unresolved" },
+      { Name: "Unassigned", Info: 0, to: "/CPD/Tickets/Unassigned" },
+      { Name: "Escalated", Info: 2, to: "/CPD/Tickets/Escalated" },
+      { Name: "All", Info: 0, to: "/CPD/Tickets/All" },
+    ],
+    to: "/CPD/Tickets/All",
+  },
+  {
+    Name: "Messages",
+    Icon: <BiMessageSquareDetail />,
+    SubMenu: [
+      { Name: "Inbox", Info: 4, to: "/CPD/Messages?location=inbox" },
+      { Name: "Sent Items", Info: 0, to: "/CPD/Messages?location=sent" },
+      { Name: "Draft", Info: 0, to: "/CPD/Messages?location=drafts" },
+    ],
+    to: "/CPD/Messages?location=inbox",
+  },
+  {
+    Name: "Reports",
+    Icon: <TbReportSearch />,
+    SubMenu: [],
+    to: "/CPD/Reports",
+  },
+  {
+    Name: "Data & Statistics",
+    Icon: <BsClipboard2Data />,
+    SubMenu: [],
+    to: "/DAS/Dashboard",
   },
 ];
