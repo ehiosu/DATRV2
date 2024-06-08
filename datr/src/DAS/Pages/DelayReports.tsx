2c71f8b6d059f7267d93e0d9fbbfd1209d214b58
ehiosu <ehiandbrothers@gmail.com>
2024-06-08 22:57:40 +0100

    update delay reports to work with general date range store

--- datr/src/DAS/Pages/DelayReports.tsx
+++ datr/src/DAS/Pages/DelayReports.tsx
@@ -1,42 +1,35 @@
 import React, { useState } from 'react'
 import { SearchPage } from "../../Reusable/SearchPage";
-import { useLocation, useParams } from "react-router";
+import { useLocation, useNavigate, useParams } from "react-router";
 import { format } from "date-fns";
 import { CiCalendar } from "react-icons/ci";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { CiCircleCheck } from "react-icons/ci";
-import { IoMdClose } from "react-icons/io";
-import { ReportsTable } from "../../CPD/Components/DataTable";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-
 import { useQuery } from "@tanstack/react-query";
 import { useAxiosClient } from "../../api/useAxiosClient";
+import {TerminalSelector} from "../Components/TerminalSelector.jsx"
 import { Skeleton } from "../../components/ui/skeleton";
+import { useTerminalStore } from '@/store/terminalstore';
+import { ArrowLeft, Download } from 'lucide-react';
+import { DelayReportsTable } from '@/CPD/Components/DataTable.js';
 export const DelayReports = () => {
     const {airline}=useParams()
-    const from=new URLSearchParams(document.location.search).get("from")
-    const to=new URLSearchParams(document.location.search).get("to")
- const [date, setDate] = useState({
-        from: from? new Date(from as string):null,
-        to: to?new Date(to as string):null,
-      });
-      const { axios } = useAxiosClient();
-      const [currentPage, setCurrentPage] = useState(1);
+    const {terminal,date,setDate}=useTerminalStore()
+    const { axios } = useAxiosClient();
+    const [currentPage, setCurrentPage] = useState(1);
+    const nav=useNavigate()
   return (
-    <section className="w-full max-h-screen overflow-y-auto p-2">
-      <SearchPage heading={airline}>
+    <section className="w-full max-h-screen overflow-y-auto p-2 relative">
+      <div onClick={()=>nav(-1)} role='button' className="absolute left-2 top-2 w-6 rounded-bl-md aspect-square bg-white  grid place-items-center">
+        <ArrowLeft className="w-4 h-4 shrink"/>
+      </div>
+      <SearchPage heading={`Delays for ${airline?.replace("_"," ")}`} SearchElement={()=><TerminalSelector/>}>
         <p className="text-[0.8275rem] text-neutral-600 font-semibold">
           Select Range:{" "}
         </p>
@@ -46,19 +39,19 @@ export const DelayReports = () => {
               id="date"
               variant={"outline"}
               className={cn(
-                "w-60 justify-start text-left font-normal dark:bg-neutral-100 bg-neutral-100 ",
+                "min-w-60 max-w-80 justify-start text-left font-normal dark:hover:bg-ncBlue hover:bg-ncBlue dark:bg-neutral-100 bg-neutral-100 ",
                 !date && "text-muted-foreground"
               )}
             >
-              <CiCalendar className="mr-2 h-4 w-4" />
+              <CiCalendar className=" h-5 w-5 shrink mr-2" />
               {date?.from ? (
                 date.to ? (
                   <>
-                    {format(date.from, "LLL dd, y")} -{" "}
-                    {format(date.to, "LLL dd, y")}
+                    {format(new Date(date.from), "LLL dd, y")} -{" "}
+                    {format(new Date(date.to), "LLL dd, y")}
                   </>
                 ) : (
-                  format(date.from, "LLL dd, y")
+                  format(new Date(date.from), "LLL dd, y")
                 )
               ) : (
                 <span>Pick a date</span>
@@ -66,42 +59,21 @@ export const DelayReports = () => {
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
+          <Calendar
               initialFocus
               mode="range"
-              defaultMonth={date?.from}
+              defaultMonth={date.from || new Date()}
               selected={date}
               onSelect={setDate}
               numberOfMonths={1}
             />
           </PopoverContent>
         </Popover>
-
+        <button className="px-4 bg-ncBlue text-sm text-white w-max flex items-center justify-center h-8 rounded-lg hover:ring-2 hover:ring-offset-4 hover:ring-blue-400 transition-all hover:font-semibold my-2">
+          Download All Reports <Download  className='w-4 h-4 shrink ml-2'/>
+        </button>
         <div className="w-full">
-          {!getReportsQuery.isError ? (
-            getReportsQuery.isSuccess ? (
-              <Accordion type="single" collapsible className="w-full">
-                {Object.keys(getReportsQuery.data).map((date) => {
-                  return (
-                    <AccordionItem className="my-3" value={date}>
-                      <AccordionTrigger>{date}</AccordionTrigger>
-                      <AccordionContent>
-                        <ReportsTable data={getReportsQuery.data[date]} />
-                      </AccordionContent>
-                    </AccordionItem>
-                  );
-                })}
-              </Accordion>
-            ) : (
-              Array.from({ length: 5 })
-                .fill(null)
-                .map((_, index) => (
-                  <Skeleton key={index} className="w-full h-12 my-3" />
-                ))
-            )
-          ) : (
-            <p>error</p>
-          )}
+         <DelayReportsTable data={[]} />
         </div>
       </SearchPage>
     </section>
