bb324407dce4c5c61f6f9e0828a1dec7429a3706
ehiosu <ehiandbrothers@gmail.com>
2024-06-24 16:37:52 +0100

    add more config options update styling

--- datr/src/CPD/Layout/CpdConfigurationLayout.jsx
+++ datr/src/CPD/Layout/CpdConfigurationLayout.jsx
@@ -6,27 +6,29 @@ export const CpdConfigurationLayout = () => {
   //   const { location } = useParams();
   //   if (!location) return redirect("/CPD/Dashboard");
   return (
-    <section className="w-full min-h-screen overflow-y-hidden">
-      <SearchPage heading={"Configuration"}>
-        <>
-          <div className="w-max px-3 border-b-2 border-neutral-200 flex items-center gap-3">
-            {/* <NavItem name={"General"} to={"/CPD/Configuration/General"} /> */}
-            <NavItem name={"Sla"} to={"/CPD/Configuration/Sla"} />
-            <NavItem name={"Tickets"} to={"/CPD/Configuration/Tickets"} />
-            <NavItem name={"User Groups"} to={"/CPD/Configuration/Groups"} />
-            {/* <NavItem
+    <section className="w-full h-full  px-6 py-2 ">
+      <p className="text-xl font-[600] my-2 text-ncBlue">Configuration</p>
+      <>
+        <div className="w-full px-3 border-b-2 border-neutral-200 flex items-center gap-3">
+          {/* <NavItem name={"General"} to={"/CPD/Configuration/General"} /> */}
+          <NavItem name={"Sla"} to={"/CPD/Configuration/Sla"} />
+          <NavItem name={"Tickets"} to={"/CPD/Configuration/Tickets"} />
+          <NavItem name={"User Groups"} to={"/CPD/Configuration/Groups"} />
+          {/* <NavItem
               name={"Conversations"}
               to={"/CPD/Configuration/Conversations"}
             /> */}
 
-            <NavItem
-              name={"Account Requests"}
-              to={"/CPD/Configuration/Accounts"}
-            />
-          </div>
-          <Outlet />
-        </>
-      </SearchPage>
+          <NavItem
+            name={"Account Requests"}
+            to={"/CPD/Configuration/Accounts"}
+          />
+          <NavItem name={"Terminals"} to={"/CPD/Configuration/Terminals"} />
+          <NavItem name={"Airlines"} to={"/CPD/Configuration/Airlines"} />
+          <NavItem name={"Routes"} to={"/CPD/Configuration/Routes"} />
+        </div>
+        <Outlet />
+      </>
     </section>
   );
 };
@@ -40,7 +42,7 @@ const NavItem = ({ to, name }) => {
       }}
       className={`flex items-center ${
         window.location.pathname === to || window.location.pathname.includes(to)
-          ? "text-blue-400 border-b-2 border-blue-400"
+          ? "text-ncBlue border-b-2 border-ncBlue"
           : "text-neutral-600"
       } h-10 hover:cursor-pointer w-max px-3 font-semibold`}
     >
