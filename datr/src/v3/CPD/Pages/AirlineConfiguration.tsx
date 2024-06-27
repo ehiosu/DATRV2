47b0d88a4843d87780883c2bd50853538fb305f0
emefienem <emefienemmichael@gmail.com>
2024-06-26 18:10:04 -0700

    Route configuration

--- datr/src/v3/CPD/Pages/AirlineConfiguration.tsx
+++ datr/src/v3/CPD/Pages/AirlineConfiguration.tsx
@@ -1,25 +1,23 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { useQuery } from "@tanstack/react-query";
-import { useAxiosClient } from "@/api/useAxiosClient";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React, { useRef } from "react";
-import { AxiosError, AxiosResponse } from "axios";
+import { useAxiosClient } from "@/api/useAxiosClient.jsx";
 import { GenericDataTable, airlineColumnDef } from "@/CPD/Components/DataTable";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { AxiosError, AxiosResponse } from "axios";
 
 export const AirlineConfiguration = () => {
-  const { axios } = useAxiosClient();
   const dialogRef = useRef<HTMLButtonElement | null>(null);
+  const { axios } = useAxiosClient();
   const getAirlineQuery = useQuery({
     queryKey: ["airlines", "all", "config"],
     queryFn: () =>
@@ -31,14 +29,6 @@ export const AirlineConfiguration = () => {
           throw err;
         }),
   });
-
-  const newAirlineFormSchema = z.object({
-    name: z.string().min(1, { message: "Name is required." }),
-  });
-
-  const newAirlineForm = useForm({
-    resolver: zodResolver(newAirlineFormSchema),
-  });
   return (
     <section className="w-full space-y-3 pb-6">
       <Dialog>
@@ -48,34 +38,19 @@ export const AirlineConfiguration = () => {
           </button>
         </DialogTrigger>
         <DialogContent>
-          <Form {...newAirlineForm}>
-            <form className="flex flex-col">
-              <FormField
-                name="name"
-                // control={newAirlineForm.control}
-                render={({ field }) => (
-                  <FormItem className="my-2">
-                    <FormLabel className="text-darkBlue font-semibold text-[0.8275rem]">
-                      Name
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Airline Name" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <button
-                type="submit"
-                // disabled={addAirlineMutation.isPending}
-                className="block h-8 mt-4 bg-darkBlue text-white hover:cursor-pointer hover:bg-purple-400 transition-colors hover:ring-2 hover:ring-offset-4 rounded-md hover:ring-purple-300 disabled:bg-neutral-100 hover:disabled:bg-neutral-100"
-              >
-                Submit
-              </button>
-            </form>
-          </Form>
+          <NewAirlineForm
+            closeDialog={() => {
+              dialogRef.current?.click();
+              getAirlineQuery.refetch();
+            }}
+          />
         </DialogContent>
       </Dialog>
+      <div className="mx-auto w-max">
+        <p className="px-1 py-1.5 text-lg font-semibold text-ncBlue border-b-2 border-b-ncBlue">
+          Airlines
+        </p>
+      </div>
       {getAirlineQuery.isSuccess && (
         <div className="max-h-[50vh] overflow-auto border-t-4 border-t-ncBlue bg-white  border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full">
           {" "}
@@ -89,5 +64,160 @@ export const AirlineConfiguration = () => {
         </div>
       )}
     </section>
+  );
+};
+
+import * as z from "zod";
+import { toast } from "sonner";
+import { MdError } from "react-icons/md";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Send } from "lucide-react";
+
+export const NewAirlineForm = ({
+  closeDialog,
+}: {
+  closeDialog: () => void;
+}) => {
+  const { axios } = useAxiosClient();
+
+  const createAirlineMutation = useMutation({
+    mutationFn: (value: {
+      airlineName: string;
+      contactAddress: string;
+      contactNumber: string;
+    }) =>
+      axios("airlines/create", {
+        method: "POST",
+        data: value,
+      })
+        .then((resp: AxiosResponse) => resp.data)
+        .catch((err: AxiosError) => {
+          throw err;
+        }),
+  });
+
+  const tryCreateNewAirline = (values: z.infer<typeof newAirlineSchema>) => {
+    const data = {
+      ...values,
+      contactAddress: values.contactAddress || "",
+      contactNumber: values.contactNumber || "",
+    };
+    toast.promise(
+      new Promise((resolve, reject) =>
+        createAirlineMutation.mutate(data, {
+          onSuccess: (data) => {
+            resolve(data);
+            closeDialog();
+          },
+          onError: (err) => reject(err),
+        })
+      ),
+      {
+        loading: "Trying to create airline...",
+        success: "Airline Created Successfully!",
+        error: (error) => {
+          return (
+            <div className="text-black flex flex-col">
+              <p className="flex flex-row items-center font-semibold text-[0.9275rem] gap-2">
+                <MdError /> Error
+              </p>
+              <p>{error.response.data.message || error.response.data.detail}</p>
+            </div>
+          );
+        },
+      }
+    );
+  };
+
+  const newAirlineSchema = z.object({
+    airlineName: z.string().min(1, { message: "Enter a valid name!" }),
+    contactAddress: z.string().optional(),
+    contactNumber: z.string().optional(),
+  });
+
+  const newAirlineForm = useForm({
+    mode: "onBlur",
+    resolver: zodResolver(newAirlineSchema),
+  });
+
+  return (
+    <Form {...newAirlineForm}>
+      <form
+        className="space-y-3"
+        onSubmit={newAirlineForm.handleSubmit(tryCreateNewAirline as any)}
+      >
+        <FormField
+          name="airlineName"
+          control={newAirlineForm.control}
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Airline Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full h-8 p-2 rounded-lg border-[1px] dark:bg-white dark:border-neutral-400 border-neutral-400 transition-all focus:border-darkBlue text-[0.77rem]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The name of the airline to be created.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          name="contactAddress"
+          control={newAirlineForm.control}
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Contact Address</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full h-8 p-2 rounded-lg border-[1px] dark:bg-white dark:border-neutral-400 border-neutral-400 transition-all focus:border-darkBlue text-[0.77rem]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The contact address of the airline (optional).
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          name="contactNumber"
+          control={newAirlineForm.control}
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Contact Number</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full h-8 p-2 rounded-lg border-[1px] dark:bg-white dark:border-neutral-400 border-neutral-400 transition-all focus:border-darkBlue text-[0.77rem]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The contact number of the airline (optional).
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <button
+          disabled={Object.keys(newAirlineForm.formState.errors).length > 0}
+          className="w-full h-8 flex flex-row items-center justify-center my-3 bg-neutral-100 hover:bg-lightPink transition-all duration-300 rounded-lg hover:text-white group disabled:bg-slate-300 disabled:cursor-not-allowed disabled:hover:text-black"
+        >
+          Submit
+          <Send className="ml-2 w-4 h-4 shrink flex flex-row items-center justify-center mt-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:duration-700 group-hover:disabled:opacity-0" />
+        </button>
+      </form>
+    </Form>
   );
 };
--- datr/src/v3/CPD/Pages/RouteConfiguration.tsx
+++ datr/src/v3/CPD/Pages/RouteConfiguration.tsx
@@ -1,29 +1,23 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { useAxiosClient } from "@/api/useAxiosClient";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React, { useRef } from "react";
-import { AxiosError, AxiosResponse } from "axios";
+import { useAxiosClient } from "@/api/useAxiosClient.jsx";
 import { GenericDataTable, routesColumnDef } from "@/CPD/Components/DataTable";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { toast } from "sonner";
-import { BiError } from "react-icons/bi";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { AxiosError, AxiosResponse } from "axios";
 
 export const RouteConfiguration = () => {
   const dialogRef = useRef<HTMLButtonElement | null>(null);
   const { axios } = useAxiosClient();
-  const queryClient = useQueryClient();
-
   const getRoutesQuery = useQuery({
     queryKey: ["routes", "all"],
     queryFn: () =>
@@ -35,107 +29,35 @@ export const RouteConfiguration = () => {
           throw err;
         }),
   });
-
-  const newRouteFormSchema = z.object({
-    name: z.string().min(1, { message: "Name is required." }),
-    abbreviation: z.string().min(1, { message: "Abbreviation is required." }),
-  });
-
-  const newRouteForm = useForm({
-    resolver: zodResolver(newRouteFormSchema),
-  });
-
-  // const addRouteMutation = useMutation({
-  //   mutationFn: (values: { name: string; abbreviation: string }) =>
-  //     axios.post("admin/routes/add", {
-  //       name: values.name.trim(),
-  //       abbreviation: values.abbreviation.trim(),
-  //     }),
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries({ queryKey: ["routes"] });
-  //     newRouteForm.reset();
-  //     dialogRef.current?.click();
-  //     toast.success("Route created successfully!");
-  //   },
-  //   onError: (error: AxiosError) => {
-  //     toast.error(
-  //       <div className="text-black flex flex-col">
-  //         <p className="flex flex-row items-center font-semibold text-[0.9275rem] gap-2">
-  //           <BiError /> Error
-  //         </p>
-  //         <p>
-  //           {(error.response?.data as { message?: string })?.message ||
-  //             "An error occurred"}
-  //         </p>
-  //       </div>
-  //     );
-  //   },
-  // });
-
-  // const onSubmit = (values: { name: string; abbreviation: string }) => {
-  //   addRouteMutation.mutate(values);
-  // };
-
   return (
     <section className="w-full space-y-3 pb-6">
       <Dialog>
         <DialogTrigger ref={dialogRef} asChild>
           <button className="mt-2 w-max text-sm bg-ncBlue px-3 py-1.5 text-white rounded-lg">
-            Add Route
+            Add Routes
           </button>
         </DialogTrigger>
-        <DialogContent className="flex flex-col">
-          <Form {...newRouteForm}>
-            <form className="flex flex-col">
-              <FormField
-                name="name"
-                // control={newRouteForm.control}
-                render={({ field }) => (
-                  <FormItem className="my-2">
-                    <FormLabel className="text-darkBlue font-semibold text-[0.8275rem]">
-                      Name
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Route Name" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="abbreviation"
-                // control={newRouteForm.control}
-                render={({ field }) => (
-                  <FormItem className="my-2">
-                    <FormLabel className="text-darkBlue font-semibold text-[0.8275rem]">
-                      Abbreviation
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Abbreviation" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <button
-                type="submit"
-                // disabled={addRouteMutation.isPending}
-                className="block h-8 mt-4 bg-darkBlue text-white hover:cursor-pointer hover:bg-purple-400 transition-colors hover:ring-2 hover:ring-offset-4 rounded-md hover:ring-purple-300 disabled:bg-neutral-100 hover:disabled:bg-neutral-100"
-              >
-                Submit
-              </button>
-            </form>
-          </Form>
+        <DialogContent>
+          <NewRouteForm
+            closeDialog={() => {
+              dialogRef.current?.click();
+              getRoutesQuery.refetch();
+            }}
+          />
         </DialogContent>
       </Dialog>
-      {/* <div className="mx-auto w-max">
+      <div className="mx-auto w-max">
         <p className="px-1 py-1.5 text-lg font-semibold text-ncBlue border-b-2 border-b-ncBlue">
-          Routes
+          Airlines
         </p>
-      </div> */}
+      </div>
       {getRoutesQuery.isSuccess && (
-        <div className="max-h-[50vh] overflow-auto border-t-4 border-t-ncBlue bg-white border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full">
+        <div className="max-h-[50vh] overflow-auto border-t-4 border-t-ncBlue bg-white  border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full">
+          {" "}
           <GenericDataTable
+            filterHeader="Route Name"
+            hasFilter
+            filterColumn="routeName"
             columns={routesColumnDef}
             data={getRoutesQuery.data}
           />
@@ -144,3 +66,248 @@ export const RouteConfiguration = () => {
     </section>
   );
 };
+import * as z from "zod";
+import { toast } from "sonner";
+import { MdError } from "react-icons/md";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Send } from "lucide-react";
+
+const NewRouteForm = ({ closeDialog }: { closeDialog: () => void }) => {
+  const { axios } = useAxiosClient();
+  const createRouteMutation = useMutation({
+    mutationFn: (value: { name: string; abbreviation: string }) =>
+      new Promise((resolve, reject) =>
+        axios("route/add", {
+          method: "POST",
+          data: value,
+        })
+          .then((resp: any) => resolve(resp))
+          .catch((err: any) => reject(err))
+      ),
+  });
+  const tryCreateNewRoute = (values: z.infer<typeof newRouteSchema>) => {
+    toast.promise(
+      new Promise((resolve, reject) =>
+        createRouteMutation.mutate(
+          { abbreviation: values.name, ...values },
+          {
+            onSuccess: (data) => {
+              resolve(data);
+              closeDialog();
+            },
+            onError: (err) => reject(err),
+          }
+        )
+      ),
+      {
+        loading: "Trying to create route...",
+        success: "Route Created Successfully!",
+        error: (error) => {
+          return (
+            <div className="text-black flex flex-col">
+              <p className="flex flex-row items-center font-semibold text-[0.9275rem] gap-2">
+                <MdError /> Error
+              </p>
+              <p>{error.response.data.message || error.response.data.detail}</p>
+            </div>
+          );
+        },
+      }
+    );
+  };
+
+  const newRouteSchema = z.object({
+    name: z.string().min(1, {
+      message: "Enter a valid name!",
+    }),
+  });
+  const newRouteForm = useForm({
+    mode: "onBlur",
+    resolver: zodResolver(newRouteSchema),
+  });
+  return (
+    <Form {...newRouteForm}>
+      <form
+        className="space-y-3"
+        onSubmit={newRouteForm.handleSubmit(tryCreateNewRoute as any)}
+      >
+        <FormField
+          name="name"
+          control={newRouteForm.control}
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full h-8 p-2 rounded-lg border-[1px] dark:bg-white dark:border-neutral-400 border-neutral-400 transition-all focus:border-darkBlue text-[0.77rem]"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The name of the route to be created.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <button
+          disabled={Object.keys(newRouteForm.formState.errors).length > 0}
+          className="w-full h-8 flex flex-row items-center justify-center my-3 bg-neutral-100 hover:bg-lightPink transition-all duration-300 rounded-lg hover:text-white group disabled:bg-slate-300 disabled:cursor-not-allowed disabled:hover:text-black"
+        >
+          Submit
+          <Send className="ml-2 w-4 h-4 shrink flex flex-row items-center justify-center mt-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:duration-700 group-hover:disabled:opacity-0" />
+        </button>
+      </form>
+    </Form>
+  );
+};
+
+// import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+// import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+// import { useAxiosClient } from "@/api/useAxiosClient";
+// import React, { useRef } from "react";
+// import { AxiosError, AxiosResponse } from "axios";
+// import { GenericDataTable, routesColumnDef } from "@/CPD/Components/DataTable";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { toast } from "sonner";
+// import { BiError } from "react-icons/bi";
+// import * as z from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+
+// export const RouteConfiguration = () => {
+//   const dialogRef = useRef<HTMLButtonElement | null>(null);
+//   const { axios } = useAxiosClient();
+//   const queryClient = useQueryClient();
+
+//   const getRoutesQuery = useQuery({
+//     queryKey: ["routes", "all"],
+//     queryFn: () =>
+//       axios("routes", {
+//         method: "GET",
+//       })
+//         .then((resp: AxiosResponse) => resp.data)
+//         .catch((err: AxiosError) => {
+//           throw err;
+//         }),
+//   });
+
+//   const newRouteFormSchema = z.object({
+//     name: z.string().min(1, { message: "Name is required." }),
+//     abbreviation: z.string().min(1, { message: "Abbreviation is required." }),
+//   });
+
+//   const newRouteForm = useForm({
+//     resolver: zodResolver(newRouteFormSchema),
+//   });
+
+//   // const addRouteMutation = useMutation({
+//   //   mutationFn: (values: { name: string; abbreviation: string }) =>
+//   //     axios.post("admin/routes/add", {
+//   //       name: values.name.trim(),
+//   //       abbreviation: values.abbreviation.trim(),
+//   //     }),
+//   //   onSuccess: () => {
+//   //     queryClient.invalidateQueries({ queryKey: ["routes"] });
+//   //     newRouteForm.reset();
+//   //     dialogRef.current?.click();
+//   //     toast.success("Route created successfully!");
+//   //   },
+//   //   onError: (error: AxiosError) => {
+//   //     toast.error(
+//   //       <div className="text-black flex flex-col">
+//   //         <p className="flex flex-row items-center font-semibold text-[0.9275rem] gap-2">
+//   //           <BiError /> Error
+//   //         </p>
+//   //         <p>
+//   //           {(error.response?.data as { message?: string })?.message ||
+//   //             "An error occurred"}
+//   //         </p>
+//   //       </div>
+//   //     );
+//   //   },
+//   // });
+
+//   // const onSubmit = (values: { name: string; abbreviation: string }) => {
+//   //   addRouteMutation.mutate(values);
+//   // };
+
+//   return (
+//     <section className="w-full space-y-3 pb-6">
+//       <Dialog>
+//         <DialogTrigger ref={dialogRef} asChild>
+//           <button className="mt-2 w-max text-sm bg-ncBlue px-3 py-1.5 text-white rounded-lg">
+//             Add Route
+//           </button>
+//         </DialogTrigger>
+//         <DialogContent className="flex flex-col">
+//           <Form {...newRouteForm}>
+//             <form className="flex flex-col">
+//               <FormField
+//                 name="name"
+//                 // control={newRouteForm.control}
+//                 render={({ field }) => (
+//                   <FormItem className="my-2">
+//                     <FormLabel className="text-darkBlue font-semibold text-[0.8275rem]">
+//                       Name
+//                     </FormLabel>
+//                     <FormControl>
+//                       <Input {...field} placeholder="Route Name" />
+//                     </FormControl>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <FormField
+//                 name="abbreviation"
+//                 // control={newRouteForm.control}
+//                 render={({ field }) => (
+//                   <FormItem className="my-2">
+//                     <FormLabel className="text-darkBlue font-semibold text-[0.8275rem]">
+//                       Abbreviation
+//                     </FormLabel>
+//                     <FormControl>
+//                       <Input {...field} placeholder="Abbreviation" />
+//                     </FormControl>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               <button
+//                 type="submit"
+//                 // disabled={addRouteMutation.isPending}
+//                 className="block h-8 mt-4 bg-darkBlue text-white hover:cursor-pointer hover:bg-purple-400 transition-colors hover:ring-2 hover:ring-offset-4 rounded-md hover:ring-purple-300 disabled:bg-neutral-100 hover:disabled:bg-neutral-100"
+//               >
+//                 Submit
+//               </button>
+//             </form>
+//           </Form>
+//         </DialogContent>
+//       </Dialog>
+//       {/* <div className="mx-auto w-max">
+//         <p className="px-1 py-1.5 text-lg font-semibold text-ncBlue border-b-2 border-b-ncBlue">
+//           Routes
+//         </p>
+//       </div> */}
+//       {getRoutesQuery.isSuccess && (
+//         <div className="max-h-[50vh] overflow-auto border-t-4 border-t-ncBlue bg-white border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full">
+//           <GenericDataTable
+//             columns={routesColumnDef}
+//             data={getRoutesQuery.data}
+//           />
+//         </div>
+//       )}
+//     </section>
+//   );
+// };
