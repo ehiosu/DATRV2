b581b6a4229bf23ff41d2f8bc9b238a4ebb18810
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-09 17:07:43 +0100

    Update CPDLayout.tsx
    allow navigation to new ticket page without redirecting user to the dashboard

--- datr/src/v3/CPD/Components/CPDLayout.tsx
+++ datr/src/v3/CPD/Components/CPDLayout.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Sidebar } from './Sidebar'
-import { Outlet, useNavigate } from 'react-router'
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router'
 import { TopNav } from './TopNav'
 import { ArrowLeft } from 'lucide-react'
+import { useAuth } from '@/api/useAuth'
 
 export const CPDLayout = () => {
   const nav=useNavigate()
+  const {access,verified}=useAuth()
+  const location = useLocation()
+  useEffect(()=>{
+  },[location])
+  const isAccessValid = access !=="access" && access
+  if (!isAccessValid && location.pathname!="/CPD/New-Ticket" ) {
+    return <Navigate to={"/"} />;}
+  if(!verified && !isAccessValid && location.pathname != "/CPD/New-Ticket") {
+    
+    console.log("not verified")
+    return <Navigate to={"/Verify"}/>}
   return (
-    <div className='flex  w-full  relative overflow-hidden '>
+    <div className='flex  w-full  relative overflow-hidden'>
         <Sidebar/>
-        <div className=" outline w-[70vw] flex flex-col  flex-1 h-screen">
+        <div className="  w-[70vw] flex flex-col  flex-1 h-screen">
           <TopNav/>
          
         <div className='bg-[#F7F7F7] flex-1  overflow-y-auto  p-2 '>
-        <button onClick={()=>{nav(-1)}} className='w-max text-xs px-3 h-max py-1.5 bg-ncBlue rounded-md text-white flex items-center'>
-            <ArrowLeft className='w-4 h-4 shrink'/> Back
-          </button>
+        {
+          isAccessValid && <button onClick={()=>{nav(-1)}} className='w-max text-xs px-3 h-max py-1.5 my-2 bg-ncBlue rounded-md text-white flex items-center'>
+          <ArrowLeft className='w-4 h-4 shrink'/> Back
+        </button>
+        }
         <Outlet/>
         {/* <div className="w-full h-[20vh] bg-ncBlue px-6 py-3">
 
