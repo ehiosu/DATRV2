a0f386d9c0b59f38a19e4b6362a9ec259406399b
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-09 17:14:23 +0100

    Update useTickets.tsx
    more rules

--- datr/src/v3/hooks/useTickets.tsx
+++ datr/src/v3/hooks/useTickets.tsx
@@ -4,26 +4,32 @@ import { DateRange } from "react-day-picker"
 import { format } from "date-fns"
 import React from "react";
 import { useAuth } from "@/api/useAuth";
-export const useTickets = (status: string, date: DateRange, page: number, size: number,setMaxPages:React.Dispatch<React.SetStateAction<number>>) => {
+export const useTickets = (status: string, date: DateRange={from:new Date(),to:new Date()}, page: number, size: number,setMaxPages:React.Dispatch<React.SetStateAction<number>>,email:string="") => {
     const { axios } = useAxiosClient();
     const {user}=useAuth()
+    const role = user.roles[user.roles.length-1]
     const isAirline=user.roles[user.roles.length-1]==="AIRLINE"
     let queryUrl =''
+    if(role==="FOU_HEAD" && status==="NEW"){
+        queryUrl=""
+    }
+    if(role !== "CPO" && !role.includes("FOU")){
     if(status==="ALL" && !date.from){
-        queryUrl=!isAirline?`tickets/all?page=${page}&size=${size}`:`tickets/airline?page=${page}&size=${size}`
+        queryUrl=!isAirline?`tickets/all?page=${page-1}&size=${size}`:`tickets/airline?page=${page-1}&size=${size}`
     }
     if(!date.from && status!=="ALL"){
-        queryUrl=!isAirline?`tickets/status?value=${status}`:`tickets/airline/status?value=${status}&page=${page}&size=${size}`
+        queryUrl=!isAirline?`tickets/status?value=${status}`:`tickets/airline/status?value=${status}&page=${page-1}&size=${size}`
         console.log(queryUrl)
     }
     // Ensure date objects are correctly instantiated
    if(date.from){
-    queryUrl=!isAirline?`date-range?start=${format(date.from as Date,'dd-MM-yyyy')}&end=${format(date.to?date.to:date.from,"dd-MM-yyyy")}&page=${page}&size=${size}`:''
+    queryUrl=!isAirline?`date-range?start=${format(date.from as Date,'dd-MM-yyyy')}&end=${format(date.to?date.to:date.from,"dd-MM-yyyy")}&page=${page-1}&size=${size}`:''
    }
+    }
+    else{
+        queryUrl=`tickets/by-creator-assignee?email=${user.email}&page=${page-1}&size=${size}`
+    }
   
-
-
-console.log(queryUrl)
     const queryFn = () => axios(queryUrl,{method:"GET"})
         .then((resp: any) => {
             setMaxPages(resp.data["totalPages"]||1)
