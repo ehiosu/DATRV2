b93de6a49fc2d0399309257482eb11b0743293f7
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-09 17:10:56 +0100

    Update Tickets.tsx

--- datr/src/v3/CPD/Pages/Tickets.tsx
+++ datr/src/v3/CPD/Pages/Tickets.tsx
@@ -7,10 +7,11 @@ import { DateRange } from 'react-day-picker'
 import { AuthorizedComponent } from '../Components/AuthorizedComponent'
 import { Plus } from 'lucide-react'
 import { useNavigate } from 'react-router'
-import { ColumnDef } from '@tanstack/react-table'
+import { NcPagination } from '@/components/ui/NcPagination'
+import { useAuth } from '@/api/useAuth'
 
 const statusColumnMap={
-  OPEN:openTicketColumnDefinition,
+  OPENED:openTicketColumnDefinition,
   ALL:generalTicketColumnDefiniton,
   RESOLVED:ResolvedTicketColumnDefinition,
   ESCALATED:UnresolvedTicketsColumnDefinition,
@@ -18,21 +19,23 @@ const statusColumnMap={
 }
 export const Tickets = () => {
   const [filter,setFilter]=useState("ALL")
-  const [currentPage,setCurrentPage]=useState(0)
+  const [currentPage,setCurrentPage]=useState(1)
   const [maxPages,setMaxPages]=useState(1)
   const [pageSize,setPageSize]=useState(10)
   const [date,setRange]=useState<DateRange>({
     from:undefined,
     to:undefined
   })
+  const {user}=useAuth()
   const query=useTickets(filter,date,currentPage,pageSize,setMaxPages)
+  console.log(query.data)
   const nav=useNavigate()
   return (
     <section className='w-full px-6 py-2 '>
     <div className="flex items-center w-full justify-between">
     <p className='text-2xl font-semibold'>Tickets</p>
-    <Select value={filter} onValueChange={(value:string)=>{
-      setCurrentPage(0)
+   {user.roles[user.roles.length-1]!== "CPO" && <Select value={filter} onValueChange={(value:string)=>{
+      setCurrentPage(1)
       setFilter(value)
     }}>
       <SelectTrigger className='w-32 h-10 bg-ncBlue dark:bg-ncBlue text-white dark:text-white hover:bg-slate-500 focus:outline-none outline-none dark:focus:outline-none ring-0 focus:ring-0 dark:focus:ring-0 dark:hover:bg-slate-500 transition'>
@@ -54,8 +57,13 @@ export const Tickets = () => {
         <SelectItem className='text-white hover:bg-slate-100/10 dark:hover:bg-slate-100/10 focus:bg-slate-100/10 dark:focus:bg-slate-100/10 dark:focus:text-white focus:text-white' value='NEW'>
           New
         </SelectItem>
+       <AuthorizedComponent roles={["SHIFT_SUPERVISOR","TERMINAL_SUPERVISOR","ADMIN"]}>
+       <SelectItem className='text-white hover:bg-slate-100/10 dark:hover:bg-slate-100/10 focus:bg-slate-100/10 dark:focus:bg-slate-100/10 dark:focus:text-white focus:text-white' value='AWAITING_ESCALATION_APPROVAL'>
+          Awaiting Escalation Approval
+        </SelectItem>
+       </AuthorizedComponent>
       </SelectContent>
-    </Select>
+    </Select>}
     </div>
     <AuthorizedComponent roles={["ADMIN","TERMINAL_HEAD","SHIFT_HEAD","CPO"]}>
             <button onClick={()=>{nav('/CPD/New-Ticket')}} className='w-40 h-9 hover:bg-slate-400 transition-all rounded-md bg-ncBlue text-white flex items-center space-x-2 justify-center ml-auto  my-2'>
@@ -64,17 +72,12 @@ export const Tickets = () => {
           </AuthorizedComponent>
    
     {
-            query.isLoading?<Skeleton className='w-full h-[60vh]'/>:query.isSuccess&& <div className="h-[60vh] overflow-auto border-t-4 border-t-ncBlue bg-white  border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full">   <GenericDataTable tableClassname='' headerClassname='rounded-lg' filterHeader='Complainant Name' columns={statusColumnMap[filter as keyof typeof statusColumnMap] as any} data={query.data||[]} filterColumn='complainantName' hasFilter/></div>
+            query.isLoading?<Skeleton className='w-full h-[60vh]'/>:query.isSuccess&& <div className="max-h-[60vh] overflow-auto border-t-4 border-t-ncBlue bg-white  border-2 border-neutral-300 rounded-lg py-1 mt-4 scroll-smooth w-full gap-y-4">   <GenericDataTable tableClassname='' headerClassname='rounded-lg' filterHeader='Complainant Name' columns={statusColumnMap[filter as keyof typeof statusColumnMap] as any} data={query.data||[]} filterColumn='complainantName' hasFilter/>
+            
+            </div>
           }
+    <NcPagination setPage={setCurrentPage} currentPage={currentPage} maxPage={maxPages} className="my-2 mx-auto w-full gap-x-3 justify-center mt-2"/>
     
-    <div className='mt-5 flex justify-center space-x-4 items-center'>
-          <button onClick={()=>setCurrentPage((state)=>state-1)}  disabled={currentPage===0} className='w-max px-5 py-1.5 text-sm rounded-lg bg-ncBlue text-white disabled:bg-slate-400'>
-            Previous
-          </button>
-          <button onClick={()=>setCurrentPage((state)=>state+1)} disabled={currentPage===maxPages-1}  className='px-5 w-max py-1.5 rounded-lg bg-ncBlue text-white text-sm disabled:bg-slate-400'>
-            Next
-          </button>
-        </div>
     </section>
   )
 }
