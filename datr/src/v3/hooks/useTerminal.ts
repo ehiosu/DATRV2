fe0b6df04ed92a8fdad2ce16041353d9054fdf2c
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-10 16:22:47 +0100

    update reusable hooks
    allow external users pull data,removing the use of axios client

--- datr/src/v3/hooks/useTerminal.ts
+++ datr/src/v3/hooks/useTerminal.ts
@@ -1,11 +1,20 @@
 import React from 'react'
 import {useAxiosClient} from "@/api/useAxiosClient.jsx"
 import { useQuery } from '@tanstack/react-query'
+import { useAuth } from '@/api/useAuth'
+import axios from 'axios'
 export const useTerminal = () => {
-    const {axios}=useAxiosClient()
+    const {axios:axiosClient}=useAxiosClient()
+    const {access}=useAuth()
+    const isExternal = access === "access" || !access
+    console.log(isExternal)
     const terminalQuery=useQuery({
         queryKey:["terminals","all"],
-        queryFn:()=>axios('terminals/active',{
+        queryFn:()=>isExternal?
+        axios("http://176.58.117.18:8080/api/terminals/active",{
+            method:"GET"
+        }).then((resp:any)=>resp.data)
+        :axiosClient('terminals/active',{
             method:"GET"
         }).then((resp:any)=>resp.data)
     })
