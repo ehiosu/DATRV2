8387a163f79a1145aff80ee5945a6f50e78e2db3
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-14 18:21:18 +0100

    Update useReports.tsx

--- datr/src/v3/hooks/useReports.tsx
+++ datr/src/v3/hooks/useReports.tsx
@@ -2,11 +2,8 @@ import { useQuery } from "@tanstack/react-query"
 import {useAxiosClient} from "@/api/useAxiosClient.jsx"
 import { DateRange } from "react-day-picker"
 import { format } from "date-fns"
-export const useReports = (terminal: string, setMaxPage:React.Dispatch<React.SetStateAction<number>>, page: number, size: number,key:string[]) => {
+export const useReports = (terminal: string, setMaxPage:React.Dispatch<React.SetStateAction<number>>, page: number, size: number,key:any[]) => {
     const { axios } = useAxiosClient();
-
-    
-
     const queryFn = () => axios(`data-entries/all?terminal=${terminal}&page=${page - 1}&size=${size}`)
         .then((resp: any) => {
             setMaxPage(resp.data["totalPages"])
