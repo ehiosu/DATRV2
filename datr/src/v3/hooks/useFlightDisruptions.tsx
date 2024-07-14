bfbacc33d72112ef7c53e13f8586c11271d101f9
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-14 18:20:19 +0100

    Update useFlightDisruptions.tsx

--- datr/src/v3/hooks/useFlightDisruptions.tsx
+++ datr/src/v3/hooks/useFlightDisruptions.tsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { useQuery } from "@tanstack/react-query"
 import {useAxiosClient} from "@/api/useAxiosClient.jsx"
 import { useAuth } from '@/api/useAuth'
-export const useFlightDisruptions = (terminal:string,currentPage:number,setMaxPage:React.Dispatch<React.SetStateAction<number>>,size:number) => {
+import { DateRange } from 'react-day-picker'
+import { format } from 'date-fns'
+export const useFlightDisruptions = (terminal:string,currentPage:number,setMaxPage:React.Dispatch<React.SetStateAction<number>>,size:number,date:DateRange) => {
     const {axios}=useAxiosClient()
     const {user}=useAuth()
     const isAirline = user.roles[user.roles.length-1]==="AIRLINE"
 const query=useQuery({
-queryKey:["fdr",terminal],
-queryFn:()=>axios(isAirline?`flight-disruption-reports/terminal/airline?terminal-name=${terminal}&airline-name=${user.airline?.replace(" ","-")}` :`flight-disruption-reports/terminal?value=${terminal}&page=${currentPage-1}&size=${size}`).then((resp:any)=>{
+queryKey:["fdr",terminal,date,currentPage],
+queryFn:()=>axios(isAirline?`flight-disruption-reports/terminal/airline?terminal-name=${terminal}&airline-name=${user.airline?.replace(" ","-")}` :`flight-disruption-reports/terminal/by-date-of-incidence?terminal=${terminal}&start-date-of-incidence=${format(date.from? date.from:new Date(),"dd-MM-yyyy")}&end-date-of-incidence=${format(date.to?date.to:new Date(),'dd-MM-yyyy')}&page=${currentPage}&size=${size}`).then((resp:any)=>{
     if(!isAirline){
     
     setMaxPage(resp.data["totalPages"])
     }
-    
+    console.log(resp.data)
     return resp.data
 })
 })
