bf7964e7a23bec102e6914179b08dee8dd32b10e
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-07-17 14:27:41 +0100

    redo notfound page

--- datr/src/CPD/Pages/NotFound.jsx
+++ datr/src/CPD/Pages/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useAuth } from "../../api/useAuth";
-
+import notfound from "/not-found.png";
 const NotFound = () => {
   const { user, access } = useAuth();
+  const home_page = home_;
   return (
-    <div className="w-full h-screen relative grid place-items-center">
+    <div className="w-full h-screen relative flex flex-col items-center justify-center">
+      <img
+        src={notfound}
+        alt="Not-found.png"
+        className="w-[35%] min-w-[320px] object-contain"
+      />
       <div className="flex flex-col justify-center text-center">
         <p>Page not Found!</p>
       </div>
